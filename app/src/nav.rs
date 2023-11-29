77d32e1057cc05c90d326d35211a60d3026eb4b1
Nathan Tranquilla <tranquilla.nathan@heb.com>
2023-11-29 16:36:05 -0400

    integrate tailwind

--- app/src/nav.rs
+++ app/src/nav.rs
@@ -3,10 +3,10 @@ use leptos::*;
 #[component]
 pub fn Nav() -> impl IntoView {
     view! {
-        <nav>
-            <a href="/summary">Summary</a>
-            <a href="/blocks">Blocks</a>
-            <a href="/accounts">Accounts</a>
+        <nav class="w-screen flex">
+            <a class="m-1.5" href="/summary">Summary</a>
+            <a class="m-1.5" href="/blocks">Blocks</a>
+            <a class="m-1.5" href="/accounts">Accounts</a>
         </nav>
     }
 }
