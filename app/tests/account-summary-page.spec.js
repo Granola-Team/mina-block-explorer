5c16e0cd3e90586aed0491fff49ed57b763c87e2
Nathan Tranquilla <n1tranquilla@gmail.com>
2023-12-05 09:25:15 -0400

    Remove fixture testing for now

--- app/tests/account-summary-page.spec.js
+++ app/tests/account-summary-page.spec.js
@@ -4,10 +4,14 @@ const { test, expect } = require('@playwright/test');
 test('account page present', async ({ page }) => {
   await page.goto('/accounts/fake-data?f=true');
 
-
-  await expect(page.getByText('Nonce')).toBeVisible();
-
-  expect(await page.locator('section').screenshot()).toMatchSnapshot();
+  await expect(page.getByText("Public Key")).toBeVisible();
+  await expect(page.getByText("Username")).toBeVisible();
+  await expect(page.getByText("Balance")).toBeVisible();
+  await expect(page.getByText("Nonce")).toBeVisible();
+  await expect(page.getByText("Receipt Chain Hash")).toBeVisible();
+  await expect(page.getByText("Delegate")).toBeVisible();
+  await expect(page.getByText("Voting For")).toBeVisible();
+  await expect(page.getByText("Pending Transactions")).toBeVisible();
 
 });
 
