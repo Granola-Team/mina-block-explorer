e1afbcf0aa77c77027bdff1b8c71f55fdd8928e3
Trevor Bernard <trevor.bernard@gmail.com>
2023-12-06 16:51:20 -0500

    holla

--- app/flake.nix
+++ app/flake.nix
@@ -48,7 +48,7 @@
           ;
 
           shellHook = ''
-            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+            export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers.outPath}
             export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
           '';
         };
