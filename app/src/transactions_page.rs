5e437e05307d6d6144ae8080bb2214bf1af76507
Nathan Tranquilla <tranquilla.nathan@heb.com>
2023-12-07 07:15:19 -0400

    Make TransactionResponse struct impl TableData trait

--- app/src/transactions_page.rs
+++ app/src/transactions_page.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::table::TableData;
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct TransactionsResponse {
     data: Vec<Transaction>
@@ -11,7 +13,8 @@ struct Transaction {
     amount: u64,
     block: Block,
     fee: u64,
-    from: String
+    from: String,
+    receiver: Receiver
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
@@ -19,4 +22,39 @@ struct Transaction {
 struct Block {
     dateTime: String
 }
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[allow(non_snake_case)]
+struct Receiver {
+    publicKey: String
+}
+
+impl TableData for TransactionsResponse {
+    fn get_columns(&self) -> Vec<String> {
+        vec![
+                String::from("Date"),
+                String::from("From"),
+                String::from("To"),
+                String::from("Hash"),
+                String::from("Fee"),
+                String::from("Amount")
+            ]
+    }
+
+    fn get_rows(&self) -> Vec<Vec<String>> {
+        let mut rows = Vec::new();
+        for transaction in &self.data {
+            let mut data = Vec::new();
+            data.push(transaction.block.dateTime.to_string());
+            data.push(transaction.from.to_string());
+            data.push(transaction.receiver.publicKey.to_string());
+            data.push(transaction.fee.to_string());
+            data.push(transaction.hash.to_string());
+            data.push(transaction.amount.to_string());
+            rows.push(data);
+        }
+        rows
+    }
+}
+
 
