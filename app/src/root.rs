99e04134c260d1c0943d031110d96d0f5d7b61f8
Nathan Tranquilla <tranquilla.nathan@heb.com>
2023-12-07 09:49:42 -0400

    Add view and route for SNARKs page

--- app/src/root.rs
+++ app/src/root.rs
@@ -3,6 +3,7 @@ use leptos_router::*;
 
 use crate::account_page::AccountSummary;
 use crate::nav::Nav;
+use crate::snarks_page::SnarksPage;
 use crate::summary_page::SummaryPage;
 use crate::latest_block_page::LatestBlocksPage;
 use crate::transactions_page::TransactionsPage;
@@ -19,6 +20,7 @@ pub fn Root() -> impl IntoView {
             <Route path="/accounts/:id" view=AccountSummary />
             <Route path="/blocks" view=LatestBlocksPage />
             <Route path="/transactions" view=TransactionsPage />
+            <Route path="/snarks" view=SnarksPage />
           </Routes>
         </Router>
       </main>
--- app/src/snarks_page.rs
+++ app/src/snarks_page.rs
@@ -1,7 +1,8 @@
 
+use leptos::*;
 use serde::{Deserialize, Serialize};
 
-use crate::table::TableData;
+use crate::{table::{TableData, Table}, api_models::MyError};
 
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct SnarksResponse {
@@ -56,5 +57,45 @@ impl TableData for SnarksResponse {
             rows.push(data);
         }
         rows
+    }
+}
+
+
+async fn load_data() -> Result<SnarksResponse, MyError> {
+    let client = reqwest::Client::new();
+    let response = client.post("https://graphql.minaexplorer.com")
+        .body(r#"{"query":"query MyQuery {\n  snarks {\n    blockHeight\n    dateTime\n    prover\n    workIds\n    block {\n      stateHash\n    }\n    fee\n  }\n}\n","variables":null,"operationName":"MyQuery"}"#)
+        .send()
+        .await
+        .map_err(|e| MyError::NetworkError(e.to_string()))?;
+
+    if response.status().is_success() {
+        let summary = response
+            .json::<SnarksResponse>()
+            .await
+            .map_err(|e| MyError::ParseError(e.to_string()))?;
+        Ok(summary)
+    } else {
+        Err(MyError::NetworkError("Failed to fetch data".into()))
+    }
+}
+
+#[component]
+pub fn SnarksPage() -> impl IntoView {
+    let resource = create_resource(|| (), |_| async move { load_data().await });
+
+    view! {
+        <h1>"SNARKs"</h1>
+        <section>
+        {move || match resource.get() {
+            None => view! {
+                <div>"Loading..." </div>
+            }.into_view(),
+            Some(Ok(data)) => view! { <Table data=data/> },
+            Some(Err(my_error)) => view! {
+                <div> { format!("Error: {:#?}", my_error)}</div>
+            }.into_view()
+        }}
+        </section>
     }
 }
