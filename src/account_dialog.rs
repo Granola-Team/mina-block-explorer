185e96bfc10d33f317640d67a5f782471854156d
Nathan Tranquilla <n1tranquilla@gmail.com>
2023-12-19 14:06:23 -0400

    Complete transaction styling

--- src/account_dialog.rs
+++ src/account_dialog.rs
@@ -1,6 +1,9 @@
 use leptos::*;
 
-// use crate::account_page::AccountSummary;
+enum Status {
+    Pending,
+    Complete
+}
 
 #[component]
 pub fn AccountDialog() -> impl IntoView {
@@ -22,6 +25,12 @@ pub fn AccountDialog() -> impl IntoView {
             "5GK2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyP8c",
             false
         ),
+    ];
+
+    let transactions = vec![
+        (Status::Pending, "2023-12-15 06:54:00", "29 minutes ago", "2n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XFG", "3n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XHI", "0.300000000", "4970037.516000000", "5PM2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE5y"),
+        (Status::Complete, "2023-12-15 06:57:00", "32 minutes ago", "2n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XFG", "3n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XHI", "0.300000000", "4970037.516000000", "5PM2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE5y"),
+        (Status::Complete, "2023-12-15 06:51:00", "38 minutes ago", "2n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XFG", "3n1YWuNHnjj6Y8C9SC1viqxXBLz99Mxks58VduA2X7uusZeS3XHI", "0.300000000", "4970037.516000000", "5PM2tkzqqK5spR2sZ7tujjqPksL45M3UUrcA4WhCkeiPtnugyE5y"),
     ];
 
     view! {
@@ -51,8 +60,64 @@ pub fn AccountDialog() -> impl IntoView {
 
                 </div>
             </section>
-            
+            <section class="flex flex-col bg-white rounded-xl flex flex-col items-stretch mt-8 p-4 h-[100%]">
+                <div class="flex justify-between w-full">
+                    <h2 class="text-xl">"Transactions"</h2>
+                    <span class="text-table-row-text-color text-xs">"Showing 5 of 110"</span>
+                </div>
+                <div class="flex flex-col md:flex-row md:flex-wrap overflow-y-auto">
+                    {transactions.into_iter()
+                        .map(|(status, date, moments_ago, from, to, fee, amount, hash)| view! {
+                            <Transaction status=status
+                                date=date.to_owned()
+                                moments_ago=moments_ago.to_owned()
+                                from=from.to_owned()
+                                to=to.to_owned()
+                                fee=fee.to_owned()
+                                amount=amount.to_owned()
+                                hash=hash.to_owned() />
+                        })
+                        .collect::<Vec<_>>()}
+                </div>
+            </section>
         </dialog>
+    }
+}
+
+#[component]
+fn Transaction(status: Status, date:String, moments_ago:String, from:String, to:String, fee:String, amount:String, hash:String) -> impl IntoView {
+
+    let img_attr = match status {
+        Status::Pending => ("assets/img/timelapse.svg","Pending"),
+        Status::Complete => ("assets/img/down-arrow.svg","Complete")
+    };
+
+    let entries = vec![
+        ("From", from),
+        ("To", to),
+        ("Fee", fee),
+        ("Amount", amount),
+        ("Hash", hash)
+    ];
+
+    view! {
+        <div class="flex justify-between w-full">
+            <div class="flex items-center">
+                <img src=img_attr.0 alt=img_attr.1 />
+                <span class="text-sm">{img_attr.1}</span>
+            </div>
+            <div class="text-xs text-slate-400">{moments_ago}</div>
+        </div>
+        {entries.into_iter()
+            .map(|(label, value)| view! {
+                <div class="w-full md:w-1/2 flex my-1">
+                    <span class="text-xs text-slate-400 w-1/4">{label}:</span>
+                    <span class="text-xs overflow-hidden text-ellipsis w-3/4">{value}</span>
+                </div>        
+            })
+            .collect::<Vec<_>>()}
+        <div class="border-b border-slate-100 my-2 h-1 w-full" />
+        
     }
 }
 
