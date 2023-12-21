f5f896fcaf7b756587a70dbe2b823a46d264f559
Nathan Tranquilla <n1tranquilla@gmail.com>
2023-12-21 15:28:33 -0400

    make dialog responsive to url changes

--- src/accounts/account_dialog.rs
+++ src/accounts/account_dialog.rs
@@ -1,6 +1,8 @@
 use leptos::*;
 use leptos_router::*;
 
+use crate::api_models::MyError;
+
 use super::functions::*;
 use super::components::*;
 
@@ -9,14 +11,20 @@ pub fn AccountDialogView() -> impl IntoView {
     let location = use_location();
     let base = get_base_page_path(location);
     let memo_params_map = use_params_map();
-    let id = memo_params_map.with(|params| params.get("id").cloned()).unwrap_or_default();
 
-    let account_resource = create_resource(|| (), move |_| {
-        let id_clone_for_async = id.clone(); // Clone the ID for the async block
-        async move { 
-            load_data(&id_clone_for_async).await
+    let account_resource = create_resource(move || memo_params_map.get(), |value| {
+        async move {
+            if let Some(id) = value.get("id").cloned() {
+                
+                let id_clone = id.clone();
+                load_data(&id_clone).await
+            } else {
+                Err(MyError::ParseError(String::from("Could not parse id parameter from url")))
+            }
         }
     });
+    
+    
 
     view! {
         {move || match account_resource.get() {
