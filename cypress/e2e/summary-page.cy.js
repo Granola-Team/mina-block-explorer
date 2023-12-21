c5e435bffe89a467b2cfb2d0789648e4b1b55d5c
Nathan Tranquilla <tranquilla.nathan@heb.com>
2023-12-21 15:39:08 -0400

    refactor cypress test

--- cypress/e2e/summary-page.cy.js
+++ cypress/e2e/summary-page.cy.js
@@ -1,6 +1,6 @@
 describe('summary page', () => {
   it('opens and closes the account overview', () => {
     cy.visit('http://localhost:5274');
-    cy.accountOverviewDialog('table tr:nth-of-type(2) a');
+    cy.openCloseAccountDialog('table tr:nth-of-type(2) a');
   })
 })
