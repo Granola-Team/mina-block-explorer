d749f99b403b6bb941607e37e6376b4da69bcc9c
Nathan Tranquilla <n1tranquilla@gmail.com>
2023-12-21 07:43:02 -0400

    update test name

--- cypress/e2e/latest-block-page.cy.js
+++ cypress/e2e/latest-block-page.cy.js
@@ -1,5 +1,5 @@
 describe('blocks page', () => {
-    it('opens and closes the account overview on the summary page', () => {
+    it('opens and closes the account overview', () => {
         cy.visit('http://localhost:5274/blocks');
         cy.accountOverviewDialog('table tr:nth-of-type(2) a');
     })
