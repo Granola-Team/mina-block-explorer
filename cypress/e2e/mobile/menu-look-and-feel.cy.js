7bffe6b9b2ae552da3af57ea8dea412133640b0b
Nathan Tranquilla <tranquilla.nathan@heb.com>
2024-01-01 10:48:26 -0400

    issue-120 add tests to capture mobile experience

--- cypress/e2e/mobile/menu-look-and-feel.cy.js
+++ cypress/e2e/mobile/menu-look-and-feel.cy.js
@@ -1,11 +1,20 @@
-const { get } = require("http");
+
+const pages = require('../../pages.json');
 
 describe('mobile menu look and feel', () => {
-    it('opens and closes the mobile menu', () => {
-        cy.viewport('iphone-xr');
-        cy.visit('/');
-        cy.get('header').screenshot();
-        cy.openMobileMenu();
-        cy.get('nav').screenshot();
+    pages.forEach(page => {
+        it(`opens and closes the mobile menu on the ${page} page`, () => {
+            cy.viewport('iphone-xr');
+            cy.visit(page);
+            cy.openMobileMenu();
+            cy.get('nav').screenshot();
+        });
     });
+
+    it('has a header',() => {
+        cy.viewport('iphone-xr');
+        cy.visit("/");
+        cy.get('header').screenshot();
+    })
+    
   })
