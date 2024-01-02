3cf8c408270fdf1823296e1bf3001a3cbb02e6ea
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-02 14:18:21 -0400

    make more idiomatic and disable links

--- src/footer/mod.rs
+++ src/footer/mod.rs
@@ -8,28 +8,33 @@ enum Icon {
     Terms,
     Support
 }
+pub struct Link {
+    label: String, 
+    href: String, 
+    icon: Icon
+}
 
 #[component]
 pub fn Footer() -> impl IntoView {
     let links = vec![
-        ("Docs", "https://docs.minaexplorer.com/", Icon::Docs),
-        ("Feedback", "https://discord.gg/Sc2JeqCPnX", Icon::Feedback),
-        ("Terms", "https://docs.minaexplorer.com/minaexplorer/website-terms-of-service", Icon::Terms),
-        ("Support", "https://docs.minaexplorer.com/minaexplorer/get-help", Icon::Support),
+        Link { label: String::from("Docs"), href: String::from("#https://docs.minaexplorer.com/"), icon: Icon::Docs},
+        Link { label: String::from("Feedback"), href: String::from("#https://discord.gg/Sc2JeqCPnX"), icon: Icon::Feedback},
+        Link { label: String::from("Terms"), href: String::from("#https://docs.minaexplorer.com/minaexplorer/website-terms-of-service"), icon: Icon::Terms},
+        Link { label: String::from("Support"), href: String::from("#https://docs.minaexplorer.com/minaexplorer/get-help"),   icon: Icon::Support},
     ];
     view! {
         <footer class="bg-main-background w-full h-12 flex flex-wrap justify-end md:grid md:grid-cols-[10%_80%_10%]">
             <div class="md:col-start-2 md:col-end-3 w-full flex justify-between sm:justify-end p-4">    
                 {links.into_iter()
-                    .map(|(name, link, icon)| view! {
-                        <a class="ml-4 flex items-center text-white text-xs uppercase hover:text-granola-orange hover:underline" href=link>
-                            {match icon {
+                    .map(|link| view! {
+                        <a class="ml-4 flex items-center text-white text-xs uppercase hover:text-granola-orange hover:underline" href=link.href>
+                            {match link.icon {
                                 Icon::Docs => view! { <DocsIcon width=12/> },
                                 Icon::Feedback => view! { <FeedbackIcon width=12 /> },
                                 Icon::Terms => view! { <TermsIcon width=12 /> },
                                 Icon::Support => view! { <SupportIcon width=12 /> },
                             }}
-                            <div class="ml-1">{name}</div>
+                            <div class="ml-1">{link.label}</div>
                         </a>
                     }).collect::<Vec<_>>()}
             </div>
