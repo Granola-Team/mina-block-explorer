d0642426df63027b913baa46073a0f3ad0d08190
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-02 13:44:36 -0400

    issue-121 update how layout abstraction works

--- src/footer/mod.rs
+++ src/footer/mod.rs
@@ -18,11 +18,11 @@ pub fn Footer() -> impl IntoView {
         ("Support", "https://docs.minaexplorer.com/minaexplorer/get-help", Icon::Support),
     ];
     view! {
-        <footer class="bg-main-background w-full h-12 flex flex-wrap justify-end">
-            <div class="w-full flex justify-between sm:max-w-md sm:justify-end p-4">    
+        <footer class="bg-main-background w-full h-12 flex flex-wrap justify-end md:grid md:grid-cols-[10%_80%_10%]">
+            <div class="md:col-start-2 md:col-end-3 w-full flex justify-between sm:justify-end p-4">    
                 {links.into_iter()
                     .map(|(name, link, icon)| view! {
-                        <a class="mr-4 flex items-center text-white text-xs uppercase hover:text-granola-orange hover:underline" href=link>
+                        <a class="ml-4 flex items-center text-white text-xs uppercase hover:text-granola-orange hover:underline" href=link>
                             {match icon {
                                 Icon::Docs => view! { <DocsIcon width=12/> },
                                 Icon::Feedback => view! { <FeedbackIcon width=12 /> },
