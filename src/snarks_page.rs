df40af506536c6810abba7f2d585a82d481aa31e
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-03 07:42:36 -0400

    issue-91 make TableData implementation more idiomatic

--- src/snarks_page.rs
+++ src/snarks_page.rs
@@ -1,9 +1,12 @@
-
 use chrono::Utc;
-use leptos::*;
 use graphql_client::{reqwest::post_graphql, GraphQLQuery};
+use leptos::*;
 
-use crate::{table::{TableData, Table}, api_models::MyError, table_section::TableSection};
+use crate::{
+    api_models::MyError,
+    table::{Table, TableData},
+    table_section::TableSection,
+};
 
 use self::snarks_query::SnarksQuerySnarks;
 
@@ -13,7 +16,7 @@ type DateTime = chrono::DateTime<Utc>;
 #[graphql(
     schema_path = "graphql/schemas/mina-explorer.graphql",
     query_path = "graphql/queries/snarks.graphql",
-    response_derives = "Serialize,PartialEq,Debug,Clone",
+    response_derives = "Serialize,PartialEq,Debug,Clone"
 )]
 pub struct SnarksQuery;
 
@@ -30,35 +33,47 @@ impl TableData for Vec<Option<SnarksQuerySnarks>> {
     }
 
     fn get_rows(&self) -> Vec<Vec<String>> {
-        self.into_iter()
+        self.iter() // Change `into_iter` to `iter` to avoid taking ownership
             .map(|opt_snark| {
                 match opt_snark {
                     Some(snark) => vec![
-                        snark.block_height.map_or(String::new(), |o| o.to_string()),
-                        snark.date_time.map_or(String::new(), |o| o.to_string()),
-                        snark.prover.clone().map_or(String::new(), |o| o.to_string()),
-                        snark.work_ids.clone().map_or(String::new(), 
-                            |o| o.iter().map(
-                                |o1| o1.map_or(String::new(), 
-                                    |o2| o2.to_string()))
-                            .collect::<Vec<_>>().join(", ")
-                        ),
-                        snark.block.clone().map_or(String::new(), |o| o.state_hash.map_or(String::new(), |o1| o1.to_string())),
-                        snark.fee.map_or(String::new(), |o| o.to_string()),
+                        snark
+                            .block_height
+                            .map_or_else(String::new, |o| o.to_string()),
+                        snark.date_time.map_or_else(String::new, |o| o.to_string()),
+                        snark
+                            .prover
+                            .as_ref()
+                            .map_or_else(String::new, |o| o.to_string()), // Borrowing with as_ref()
+                        snark
+                            .work_ids
+                            .as_ref()
+                            .map_or_else(String::new, |work_ids| {
+                                work_ids
+                                    .iter()
+                                    .map(|o| o.map_or_else(String::new, |o1| o1.to_string()))
+                                    .collect::<Vec<_>>()
+                                    .join(", ")
+                            }),
+                        snark.block.as_ref().map_or_else(String::new, |o| {
+                            o.state_hash
+                                .as_ref()
+                                .map_or_else(String::new, |o| o.to_string())
+                        }),
+                        snark.fee.map_or_else(String::new, |o| o.to_string()),
                     ],
                     None => vec![],
                 }
-            }).collect::<Vec<_>>()
-        
+            })
+            .collect::<Vec<_>>()
     }
 }
-
 
 async fn load_data() -> Result<snarks_query::ResponseData, MyError> {
     let url = "https://graphql.minaexplorer.com";
     let variables = snarks_query::Variables {
         sort_by: snarks_query::SnarkSortByInput::BLOCKHEIGHT_DESC,
-        limit: Some(25)
+        limit: Some(25),
     };
 
     let client = reqwest::Client::new();
@@ -73,16 +88,15 @@ async fn load_data() -> Result<snarks_query::ResponseData, MyError> {
     } else {
         Ok(response.data.unwrap())
     }
-
 }
 
 #[component]
 pub fn SnarksPage() -> impl IntoView {
     let resource = create_resource(|| (), |_| async move { load_data().await });
 
-    view! {        
+    view! {
         {move || match resource.get() {
-            Some(Ok(data)) => view! { 
+            Some(Ok(data)) => view! {
                 <TableSection section_heading="SNARKs".to_owned()>
                     <Table data=data.snarks/>
                 </TableSection>
