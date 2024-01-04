ce1bd7b22de23f7c2cff9cd81beeb3dda25eb456
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-04 08:47:38 -0400

    Add new NoIcon

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -94,3 +94,13 @@ pub fn StakingIcon(#[prop(default = 20)] width: i8) -> impl IntoView {
         </svg>
     }
 }
+
+#[component]
+pub fn NoIcon(#[prop(default = 20)] width: i8) -> impl IntoView {
+    view! {
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width=width>
+            <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+        </svg>
+    }
+}
+
