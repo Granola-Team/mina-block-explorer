1b03ee62ab91668559c3c9c587e09ce27f66aadf
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-04 10:58:50 -0400

    issue-122 increase timeouts

--- cypress/e2e/all/account-dialog.cy.js
+++ cypress/e2e/all/account-dialog.cy.js
@@ -2,6 +2,6 @@ describe('account dialog', () => {
     it('has a transaction section', () => {
         cy.visit('summary/accounts/B62qrCz3ehCqi8Pn8y3vWC9zYEB9RKsidauv15DeZxhzkxL3bKeba5h')
         cy.get('section h2').contains('Transactions',{timeout:10000})
-        cy.get('section h2').contains('SNARK Jobs',{timeout:15000})
+        cy.get('section h2').contains('SNARK Jobs',{timeout:20000})
     })
 })
