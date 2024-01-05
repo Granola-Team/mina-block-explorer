d5b3a62e1ed45d0b1a0566edaaaa62914fab6031
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-05 13:54:09 -0400

    extract table trait

--- src/snarks/page.rs
+++ src/snarks/page.rs
@@ -1,54 +1,13 @@
 use super::functions::*;
-use super::graphql::*;
 use crate::common::components::*;
 use leptos::*;
-use snarks_query::SnarksQuerySnarks;
 
-impl TableData for Vec<Option<SnarksQuerySnarks>> {
-    fn get_columns(&self) -> Vec<String> {
-        ["Height", "Date", "Prover", "Work Ids", "State Hash", "Fee"]
-            .iter()
-            .map(ToString::to_string)
-            .collect::<Vec<_>>()
-    }
-
-    fn get_rows(&self) -> Vec<Vec<String>> {
-        self.iter()
-            .map(|opt_snark| match opt_snark {
-                Some(snark) => vec![
-                    snark
-                        .block_height
-                        .map_or_else(String::new, |o| o.to_string()),
-                    snark.date_time.map_or_else(String::new, |o| o.to_string()),
-                    snark
-                        .prover
-                        .as_ref()
-                        .map_or_else(String::new, |o| o.to_string()),
-                    snark
-                        .work_ids
-                        .as_ref()
-                        .map_or_else(String::new, |work_ids| {
-                            work_ids
-                                .iter()
-                                .map(|o| o.map_or_else(String::new, |o1| o1.to_string()))
-                                .collect::<Vec<_>>()
-                                .join(", ")
-                        }),
-                    snark.block.as_ref().map_or_else(String::new, |o| {
-                        o.state_hash
-                            .as_ref()
-                            .map_or_else(String::new, |o| o.to_string())
-                    }),
-                    snark.fee.map_or_else(String::new, |o| o.to_string()),
-                ],
-                None => vec![],
-            })
-            .collect::<Vec<_>>()
-    }
-}
 
 #[component]
 pub fn SnarksPage() -> impl IntoView {
+
+    
+
     let resource = create_resource(|| (), |_| async move { load_data(10, None).await });
 
     view! {
