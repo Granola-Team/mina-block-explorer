fb0d1ae5a763bf2026424e45990904e0626e0584
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-07 09:07:54 -0400

    fix flow of elements on spotlight page

--- src/common/spotlight.rs
+++ src/common/spotlight.rs
@@ -32,9 +32,17 @@ pub fn Spotlight(
         </div>
         <div class="@3xl:grid @3xl:grid-cols-[10rem_auto_10rem] bg-white rounded-xl flex flex-col items-stretch mt-8 p-4">
             {summary_items.into_iter()
-                .map(|entry| view! {
-                    <SpotlightRow entry=entry />
-                })
+                .map(|entry| view! { <SpotlightRow entry=entry /> }) 
+                .collect::<Vec<_>>() 
+                .as_slice() 
+                .chunks(2) 
+                .map(|chunk| {
+                    view! {
+                        <div class="@3xl:col-start-2 @3xl:col-end-3 @7xl:flex">
+                            { chunk.get(0) } {chunk.get(1)}
+                        </div>
+                    }
+                }) // Wrap each chunk in a view
                 .collect::<Vec<_>>()}
 
         </div>
@@ -60,13 +68,13 @@ fn SpotlightRow(entry: SpotlightEntry) -> impl IntoView {
         None => format!(
             "{} {}",
             value_class_str_base.to_owned(),
-            "w-3/4 text-ellipsis overflow-hidden"
+            "w-1/2 text-ellipsis overflow-hidden"
         ),
     };
 
     view! {
-        <div class="@3xl:col-start-2 @3xl:col-end-3 flex flex-col items-start md:flex-row md:items-baseline md:justify-start">
-            <span class="w-1/4 text-slate-400 text-sm whitespace-nowrap">{entry.label}:</span>
+        <div class="flex flex-row items-start items-baseline justify-start w-full @7xl:max-w-[50%]">
+            <span class="w-1/4 min-w-[150px] text-slate-400 text-sm whitespace-nowrap">{entry.label}:</span>
             <span class=value_class_str>{entry.value}</span>
         </div>
     }
