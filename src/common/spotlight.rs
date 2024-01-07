4640523f6044aa8640f0d6dc98394e54f229aad3
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-07 12:37:47 -0400

    add html id attributes to support tests

--- src/common/spotlight.rs
+++ src/common/spotlight.rs
@@ -19,18 +19,18 @@ pub fn Spotlight(
     children: Children,
 ) -> impl IntoView {
     view! {
-        <div class="@3xl:grid @3xl:grid-cols-[10rem_5rem_auto_10rem] @3xl:grid-rows-[2.5rem_2.5rem] @3xl:gap-x-[2rem] @3xl:h-auto flex flex-col items-center mt-16 bg-light-granola-orange rounded-3xl h-36">
-            <div class="@3xl:col-start-2 @3xl:col-end-3 @3xl:row-start-1 @3xl:row-end-2 w-20 h-20 rounded-full bg-main-background flex justify-center items-center translate-y-[-25%] text-granola-orange">
+        <div id="spotlight-heading" class="@3xl:grid @3xl:grid-cols-[10rem_5rem_auto_10rem] @3xl:grid-rows-[2.5rem_2.5rem] @3xl:gap-x-[2rem] @3xl:h-auto flex flex-col items-center mt-16 bg-light-granola-orange rounded-3xl h-36">
+            <div id="spotlight-icon" class="@3xl:col-start-2 @3xl:col-end-3 @3xl:row-start-1 @3xl:row-end-2 w-20 h-20 rounded-full bg-main-background flex justify-center items-center translate-y-[-25%] text-granola-orange">
                 {children()}
             </div>
-            <div class="@3xl:col-start-3 text-granola-orange text-base text-bold text-ellipsis w-10/12 overflow-hidden text-center @3xl:text-left">
+            <div id="spotlight-id" class="@3xl:col-start-3 text-granola-orange text-base text-bold text-ellipsis w-10/12 overflow-hidden text-center @3xl:text-left">
                 {id}
             </div>
-            <div class="@3xl:col-start-3 @3xl:row-start-2 text-slate-400 text-sm">
+            <div id="spotlight-meta" class="@3xl:col-start-3 @3xl:row-start-2 text-slate-400 text-sm">
                 {meta}
             </div>
         </div>
-        <div class="@3xl:grid @3xl:grid-cols-[10rem_auto_10rem] bg-white rounded-xl flex flex-col items-stretch mt-8 p-4">
+        <div id="spotlight-info" class="@3xl:grid @3xl:grid-cols-[10rem_auto_10rem] bg-white rounded-xl flex flex-col items-stretch mt-8 p-4">
             {summary_items.into_iter()
                 .map(|entry| view! { <SpotlightRow entry=entry /> }) 
                 .collect::<Vec<_>>() 
