a04347cd3015a8e31bc4bcc72e46d5a24ce52348
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-08 11:03:11 -0400

    force click

--- cypress/e2e/desktop/latest-block-page.cy.js
+++ cypress/e2e/desktop/latest-block-page.cy.js
@@ -1,7 +1,7 @@
 describe('blocks page', () => {
     it('opens and closes the account overview', () => {
         cy.visit('/blocks');
-        cy.openAccountDialog('table tr:nth-of-type(2) > a');
+        cy.openAccountDialog('table tr:nth-of-type(2) a');
         cy.closeAccountDialog();
     })
   })
