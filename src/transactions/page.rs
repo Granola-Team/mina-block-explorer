bb55a809e556096f05c9fc735d342820119f148c
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-09 14:49:11 -0400

    create transaction spotlight page without any information details

--- src/transactions/page.rs
+++ src/transactions/page.rs
@@ -3,6 +3,10 @@ use leptos_router::*;
 
 use super::functions::*;
 use super::components::*;
+use crate::common::components::*;
+use crate::common::functions::*;
+use crate::common::spotlight::*;
+use crate::icons::*;
 
 #[component]
 pub fn TransactionsPage() -> impl IntoView {
@@ -19,7 +23,7 @@ pub fn TransactionsPage() -> impl IntoView {
 #[component]
 pub fn TransactionSpotlightPage() -> impl IntoView {
     let memo_params_map = use_params_map();
-    let _resource = create_resource(
+    let resource = create_resource(
         move || memo_params_map.get(),
         |value| async move {
             let state_hash = value.get("id");
@@ -27,5 +31,27 @@ pub fn TransactionSpotlightPage() -> impl IntoView {
         },
     );
 
-    view! { <span>"spotlight page"</span> }
+    view! { 
+        { move || match resource.get() {
+            Some(Ok(data)) => {
+                match data.transactions.first() {
+                    Some(Some(transaction)) => {
+                        let state_hash = get_hash(transaction);
+                        let date_time = get_block_datetime(transaction);
+                        let summary_items=Vec::new();
+                        view!{
+                            <section class="@container md:col-start-2 md:col-end-3 md:rounded-lg bg-table-section p-0 md:p-4 mb-2">
+                                <h1 class="md:rounded-lg h-16 pl-8 text-xl bg-table-section flex justify-start items-center">"Transaction Spotlight"</h1>
+                                <Spotlight summary_items=summary_items id=state_hash meta=format!("{} ({})", date_time, print_time_since(&date_time)) >
+                                    <TransactionIcon width=40/>
+                                </Spotlight>
+                            </section>
+                        }.into_view()
+                    }
+                    _ => view! { <NullView />}
+                }
+            },
+            _ => view! { <NullView /> },
+        }}
+    }
 }
