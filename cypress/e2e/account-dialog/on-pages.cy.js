a69676a6d50780c0f8672c9fb36e6a19e8dadd10
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-20 17:13:15 -0400

    slow down automation to fix very odd speed-of-machine error

--- cypress/e2e/account-dialog/on-pages.cy.js
+++ cypress/e2e/account-dialog/on-pages.cy.js
@@ -13,6 +13,7 @@ describe('account dialog', () => {
     pages_with_account_dialog.forEach(page => {
       columns.forEach(col => it(`is launched on ${page} by click link in '${col}'`,() => {
         cy.visit(page);
+        cy.wait(1000);
         cy.openAccountDialog(1, col, 'Blocks');
         cy.closeAccountDialog();
       }));
