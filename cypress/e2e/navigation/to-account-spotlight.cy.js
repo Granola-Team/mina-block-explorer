1302c65898c687cfe90ae4b1ea50e0cc6734b6bd
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-25 08:59:20 -0400

    Revert "speed up click tests"
    This reverts commit 88b8d5f2494fe9a90ace242fca2bfb38e594ec5b.

--- cypress/e2e/navigation/to-account-spotlight.cy.js
+++ cypress/e2e/navigation/to-account-spotlight.cy.js
@@ -10,9 +10,9 @@ describe('account spotlight', () => {
 
     dialogs.forEach(({ origin, selector }) => it(`is navigated to from ${origin}`,() => {
         cy.visit(origin);
-        cy.wait(500);
+        cy.wait(1000);
         cy.get(selector, {timeout: 10000}).first().click({force:true});
-        cy.wait(500);
+        cy.wait(1000);
         cy.url().should('include','/accounts/', {timeout: 10000})
     }));
 
