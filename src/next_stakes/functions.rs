34b331d4ad095bb37582650321c6f5e551bb9917
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-25 11:01:06 -0400

    add functions for consumption in table traits mod

--- src/next_stakes/functions.rs
+++ src/next_stakes/functions.rs
@@ -3,6 +3,41 @@ use graphql_client::reqwest::post_graphql;
 use crate::common::models::*;
 use crate::next_stakes::graphql::{next_stakes_query, *};
 
+use next_stakes_query::NextStakesQueryNextstakes;
+
+pub fn get_public_key(nextstakes: &NextStakesQueryNextstakes) -> String {
+    nextstakes
+        .public_key
+        .as_ref()
+        .map_or("".to_string(), |o| o.to_string())
+}
+
+pub fn get_balance(nextstakes: &NextStakesQueryNextstakes) -> String {
+    nextstakes.balance.map_or("".to_string(), |o| o.to_string())
+}
+
+pub fn get_delegate(nextstakes: &NextStakesQueryNextstakes) -> String {
+    nextstakes
+        .delegate
+        .as_ref()
+        .map_or("".to_string(), |o| o.to_string())
+}
+
+pub fn get_delegators_count(nextstakes: &NextStakesQueryNextstakes) -> String {
+    nextstakes
+        .next_delegation_totals
+        .as_ref()
+        .and_then(|o| o.count_delegates)
+        .map_or("0".to_string(), |o| o.to_string())
+}
+
+pub fn get_ledger_hash(nextstakes: &NextStakesQueryNextstakes) -> String {
+    nextstakes
+        .ledger_hash
+        .as_ref()
+        .map_or("".to_string(), |o| o.to_string())
+}
+
 pub async fn load_data(limit: i64) -> Result<next_stakes_query::ResponseData, MyError> {
     let url = "https://graphql.minaexplorer.com";
     let variables = next_stakes_query::Variables {
