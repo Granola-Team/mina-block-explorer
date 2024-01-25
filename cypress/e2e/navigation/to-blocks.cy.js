8381c9406fbe9fc2ef803b79c123330bcf725196
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-25 09:45:15 -0400

    update test to display origin

--- cypress/e2e/navigation/to-blocks.cy.js
+++ cypress/e2e/navigation/to-blocks.cy.js
@@ -3,7 +3,7 @@ describe('block page', () => {
         origin: '/accounts/B62qq3TQ8AP7MFYPVtMx5tZGF3kWLJukfwG1A1RGvaBW1jfTPTkDBW6',
         dest:"blocks",
         href:"/blocks?account=B62qq3TQ8AP7MFYPVtMx5tZGF3kWLJukfwG1A1RGvaBW1jfTPTkDBW6"
-    }].forEach(({origin, dest, href}) => it(`is navigated to from ${dest}`,() => {
+    }].forEach(({origin, dest, href}) => it(`is navigated to from ${origin}`,() => {
         cy.visit(origin);
         cy.get('a').contains("See all block production", {timeout: 60000}).click();
         cy.url().should('contain', href);
