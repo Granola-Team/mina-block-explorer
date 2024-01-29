0fb401116036ad29e0cda8e17b7da526e15d0dc3
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-29 11:31:44 -0400

    paging on snarks

--- src/snarks/page.rs
+++ src/snarks/page.rs
@@ -2,6 +2,7 @@ use super::functions::*;
 use crate::common::components::*;
 use crate::common::search::*;
 use crate::common::table::*;
+use crate::common::functions::*;
 use leptos::*;
 use leptos_router::use_query_map;
 
@@ -16,17 +17,24 @@ pub fn SnarksPage() -> impl IntoView {
             if public_key.is_none() {
                 public_key = value.get("query");
             }
-            load_data(10, public_key.cloned(), None).await
+            load_data(50, public_key.cloned(), None).await
         },
     );
+
+    let records_per_page = 10;
+    let (current_page, set_current_page) = create_signal(1);
 
     view! {
         <SearchBar placeholder="Exact search for prover".to_string()/>
         <PageContainer>
             <TableSection section_heading="SNARKs".to_owned() controls=|| ().into_view()>
             {move || match resource.get() {
-                Some(Ok(data)) => view! {
-                    <Table data=data.snarks/>
+                Some(Ok(data)) => {
+                    let pag = build_pagination(data.snarks.len(), records_per_page, current_page.get(), set_current_page);
+                    let subset = get_subset(&data.snarks, records_per_page, current_page.get()-1);
+                    view! {
+                        <Table data=subset pagination=pag/>
+                    }
                 },
                 None => view! { <Table data=LoadingPlaceholder{} />},
                 _ => view! { <span /> }.into_view()
