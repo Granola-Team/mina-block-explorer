3d17c9ff0b7ab339b653c5f15a2dcc79c2779235
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-01-30 14:44:52 -0400

    cargo fmt

--- src/accounts/table_traits.rs
+++ src/accounts/table_traits.rs
@@ -1,8 +1,7 @@
-use leptos::*;
 use crate::common::{functions::*, models::*, table::TableData};
+use leptos::*;
 
 use super::models::AllAccountSummary;
-
 
 impl TableData for Vec<Option<AllAccountSummary>> {
     fn get_columns(&self) -> Vec<String> {
@@ -20,17 +19,21 @@ impl TableData for Vec<Option<AllAccountSummary>> {
         self.iter()
             .map(|all_acct_sum_opt| match all_acct_sum_opt {
                 Some(all_account_sum) => vec![
-                    convert_to_link(all_account_sum.pk.to_string(),format!("/accounts/{}",all_account_sum.pk.to_string())),
+                    convert_to_link(
+                        all_account_sum.pk.to_string(),
+                        format!("/accounts/{}", all_account_sum.pk.to_string()),
+                    ),
                     convert_to_span(all_account_sum.username.to_string()),
                     convert_to_pill(all_account_sum.balance.to_string(), PillVariant::Green),
                     convert_to_pill(all_account_sum.nonce.to_string(), PillVariant::Grey),
-                    convert_to_link(all_account_sum.delegate.to_string(),format!("/accounts/{}",all_account_sum.delegate.to_string())),
+                    convert_to_link(
+                        all_account_sum.delegate.to_string(),
+                        format!("/accounts/{}", all_account_sum.delegate.to_string()),
+                    ),
                     convert_to_span(false.to_string()),
                 ],
-                None => vec![]
+                None => vec![],
             })
             .collect::<Vec<_>>()
-
-                
     }
 }
