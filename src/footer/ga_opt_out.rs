19206638053aa45ac436e91d8b497a8b432362a5
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-02-15 10:42:52 -0400

    issue-474 add ability to opt out of google analytics

---
+++ src/footer/ga_opt_out.rs
@@ -0,0 +1,41 @@
+use leptos::*;
+use leptos_use::{storage::{use_local_storage, StringCodec}};
+use crate::icons::*;
+
+#[component]
+pub fn GAOptOut() -> impl IntoView {
+
+    let (flag, set_flag, _remove_flag) = use_local_storage::<bool, StringCodec>("ga-opt-out");
+    let (first_load, set_first_load) = create_signal(true);
+    
+
+    create_effect(move |_| {
+        flag.get();
+        match first_load.get() {
+            true => {
+                let _ = set_first_load.set(false);
+            },
+            false => {
+                // so that analytics can be conditionally loaded back into index.html
+                let _ = window().location().reload();
+            }
+        }
+    });
+
+    view! {
+        <button 
+            class="ml-1 sm:ml-4 flex items-center text-white text-xs uppercase hover:text-granola-orange hover:underline"
+            on:click=move |_| set_flag.update(|f| *f = !*f)
+            title={move || match flag.get() { true => "You have opted out of google analytics tracking", false => "Google analytics tracking is active"}}>
+            { move || match flag.get() {
+                true => view! {
+                    <NoSymbol width=12/>
+                },
+                false => view! {
+                    <CheckCircleIcon width=12/>
+                }
+            }}
+            <div class="ml-1 whitespace-nowrap overflow-hidden text-ellipsis">"Web Analytics"</div>
+        </button>
+    }
+}
