e40d02b59acbbf92d6abed4e140eaef943c2f484
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-02-15 11:05:34 -0400

    formatting changes

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -471,8 +471,19 @@ pub fn ClipboardIcon(#[prop(default = 20)] width: i8) -> impl IntoView {
 #[component]
 pub fn NoSymbol(#[prop(default = 20)] width: i8) -> impl IntoView {
     view! {
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width=width>
-            <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            width=width
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636"
+            ></path>
         </svg>
     }
 }
