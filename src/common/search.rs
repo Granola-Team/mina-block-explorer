31ea6c976c96bbac1dae5df20dc81453b906185b
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-02-16 18:21:48 -0400

    formatting

--- src/common/search.rs
+++ src/common/search.rs
@@ -22,48 +22,44 @@ fn SearchInput(placeholder: String) -> impl IntoView {
     let (input, set_input) = create_signal(None);
     let debounced: Signal<Option<String>> = signal_debounced(input, 500.0);
 
-    create_effect(move |last_query| {
-        match (query.get(), last_query) {
-            (Some(query_str), Some(Some(last_query_str))) => {
-                if query_str != last_query_str {
-                    logging::log!("Setting input from query {}", query_str);
-                    set_input.set(Some(query_str.clone()));
-                    return Some(query_str)
-                }
-                Some(last_query_str)
-            },
-            (Some(query_str), Some(None)) => {
+    create_effect(move |last_query| match (query.get(), last_query) {
+        (Some(query_str), Some(Some(last_query_str))) => {
+            if query_str != last_query_str {
                 logging::log!("Setting input from query {}", query_str);
                 set_input.set(Some(query_str.clone()));
-                return Some(query_str)
-            },
-            _ => None
+                return Some(query_str);
+            }
+            Some(last_query_str)
         }
+        (Some(query_str), Some(None)) => {
+            logging::log!("Setting input from query {}", query_str);
+            set_input.set(Some(query_str.clone()));
+            return Some(query_str);
+        }
+        _ => None,
     });
 
-    create_effect(move |prev_value| {
-        match (debounced.get(), prev_value) {
-           (Some(input_value), Some(Some(prev_value_str))) => {
-                if input_value != prev_value_str {
-                    logging::log!("Setting query from input {}", input_value);
-                    match input_value.len() > 0 {
-                        true => set_query.set(Some(input_value.to_string())),
-                        false => set_query.set(None),
-                    }
-                    return Some(input_value);
-                }         
-                Some(input_value)
-           },
-           (Some(input_value), Some(None)) => {
+    create_effect(move |prev_value| match (debounced.get(), prev_value) {
+        (Some(input_value), Some(Some(prev_value_str))) => {
+            if input_value != prev_value_str {
                 logging::log!("Setting query from input {}", input_value);
                 match input_value.len() > 0 {
                     true => set_query.set(Some(input_value.to_string())),
                     false => set_query.set(None),
                 }
                 return Some(input_value);
-           }
-           _ => None
+            }
+            Some(input_value)
         }
+        (Some(input_value), Some(None)) => {
+            logging::log!("Setting query from input {}", input_value);
+            match input_value.len() > 0 {
+                true => set_query.set(Some(input_value.to_string())),
+                false => set_query.set(None),
+            }
+            return Some(input_value);
+        }
+        _ => None,
     });
 
     view! {
@@ -77,6 +73,7 @@ fn SearchInput(placeholder: String) -> impl IntoView {
                     false => set_query.set(None),
                 }
             }
+
             prop:value=move || query.get().unwrap_or("".to_string())
             placeholder=move || query.get().unwrap_or(placeholder.clone())
             on:input=move |event| set_input.update(|e| *e = Some(event_target_value(&event)))
