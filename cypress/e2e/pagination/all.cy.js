71752ecf92b390ac2bf013dee83c50b93acf5837
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-02-20 11:16:12 -0400

    add pagination tests

---
+++ cypress/e2e/pagination/all.cy.js
@@ -0,0 +1,30 @@
+suite(["@CI"],'pagination',() => {
+
+    ['/blocks'].forEach(page => it(`defaults to first page, on ${page}`,() => {
+        cy.visit(page);
+        cy.get('.pagination-controls', {timeout: 30000}).find('button').last().as('next');
+        cy.get('.pagination-controls', {timeout: 30000}).find('button').first().as('prev');
+        cy.get('.pagination-controls', {timeout: 30000}).find('.current-page').as('currentPage');
+
+        // initial check
+        cy.get('@prev').should('be.disabled');
+        cy.get('@next').should('not.be.disabled');
+        cy.get('@currentPage').should('contain',1);
+
+        // next page
+        cy.get('@next').click();
+        cy.wait(1000);
+        cy.get('@prev').should('not.be.disabled');
+        cy.get('@next').should('not.be.disabled');
+        cy.get('@currentPage').should('contain',2);    
+
+        // number click (last page)
+        cy.get('.pagination-controls', {timeout: 30000}).find('button').contains('5').click();
+        cy.wait(1000);
+        cy.get('@prev').should('not.be.disabled');
+        cy.get('@next').should('be.disabled');
+        cy.get('@currentPage').should('contain',5);    
+
+    }));
+
+});
