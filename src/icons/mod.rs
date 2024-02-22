aad9fdafef41d7fbf2c45861907422b7bb5a57cc
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-02-22 10:58:57 -0400

    add new fee transfer icon

--- src/icons/mod.rs
+++ src/icons/mod.rs
@@ -552,3 +552,23 @@ pub fn AddressIcon(#[prop(default = 20)] width: i8) -> impl IntoView {
         </svg>
     }
 }
+
+#[component]
+pub fn FeeTransferIcon(#[prop(default = 20)] width: i8) -> impl IntoView {
+    view! {
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            width=width
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7.5 21 3 16.5m0 0L7.5 12M3 16.5h13.5m0-13.5L21 7.5m0 0L16.5 12M21 7.5H7.5"
+            ></path>
+        </svg>
+    }
+}
