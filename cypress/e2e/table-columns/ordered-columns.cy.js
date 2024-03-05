06a644281278f6a59393b449ed4ed59665151e4f
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-05 11:21:54 -0400

    move transactions table test to new test file

--- cypress/e2e/table-columns/ordered-columns.cy.js
+++ cypress/e2e/table-columns/ordered-columns.cy.js
@@ -1,3 +1,14 @@
+suite(["@CI"],'transactions table', () => {
+
+    let pages = ['/transactions'];
+    let columns = ['Height', 'Age', 'From', 'To', 'Nonce', 'Hash', 'Fee', 'Amount'];
+
+    pages.forEach(page => it(`on ${page} includes correct columns`, () => {
+        cy.visit(page);
+        cy.tableHasOrderedColumns('Transactions', columns);
+    }));
+});
+
 suite(["@CI"],'blocks table', () => {
 
     let pages = ['/blocks', '/summary', '/'];
