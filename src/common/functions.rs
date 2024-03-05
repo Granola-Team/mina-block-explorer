0e8013075d0e73ef3cea0abc3731f00033a3e740
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-05 15:03:32 -0400

    format

--- src/common/functions.rs
+++ src/common/functions.rs
@@ -277,15 +277,26 @@ pub fn convert_to_link(data: String, href: String) -> HtmlElement<html::AnyEleme
 
 pub fn convert_to_ellipsis(text: String) -> HtmlElement<html::AnyElement> {
     let parts_base = "overflow-hidden flex-initial";
-    let midpoint = text.len()/2;
+    let midpoint = text.len() / 2;
     let first = text.chars().take(midpoint).collect::<String>();
-    let last = text.chars().rev().take(midpoint).collect::<String>().chars().rev().collect::<String>();
+    let last = text
+        .chars()
+        .rev()
+        .take(midpoint)
+        .collect::<String>()
+        .chars()
+        .rev()
+        .collect::<String>();
     view! {
         <div class="flex items-baseline justify-center">
-            <div class=format!("{} break-all", parts_base)>
-                {first}
-            </div>
-            <div style="direction: rtl;" class=format!(r#"{} whitespace-nowrap text-ellipsis after:content-['\200E']"#, parts_base)>
+            <div class=format!("{} break-all", parts_base)>{first}</div>
+            <div
+                style="direction: rtl;"
+                class=format!(
+                    r#"{} whitespace-nowrap text-ellipsis after:content-['\200E']"#,
+                    parts_base,
+                )
+            >
                 {last}
             </div>
         </div>
