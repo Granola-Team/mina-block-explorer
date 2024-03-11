e7db89fa699d881e2f5e7cb8d8bfa6eec81984d7
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-11 10:23:55 -0300

    update blocks tests

--- cypress/e2e/blocks/blocks-table.cy.js
+++ cypress/e2e/blocks/blocks-table.cy.js
@@ -2,20 +2,28 @@ suite(["@CI"],'blocks table', () => {
 
     let pages = ['/blocks', '/summary', '/'];
 
-    pages.forEach(page => it(`on ${page} includes non-canonical blocks when toggled is checked`, () => {
+    pages.forEach(page => it(`on ${page} includes canonical blocks according to selection`, () => {
         cy.visit(page);
-        cy.get('label').contains('Include Non-Canonical').as('toggle');
-        cy.get('@toggle').click();
-        cy.url().should('include', 'include_non_canonical=true');
+        cy.get('select#canonical-selection').as('menu');
+        
+        cy.get('@menu').select('Canonical');
+        cy.url().should('include', 'canonical=true');
         cy.wait(500);
         cy.contains('section','Blocks').find('table tr:not(:has(th)) span', {timeout: 60000}).as('tableRows')
-        cy.get('@tableRows').should('have.class', 'bg-status-failed');
+        cy.get('@tableRows').should('not.have.class', 'bg-status-failed');
         cy.get('@tableRows').should('have.class', 'bg-status-success');
-        cy.get('@toggle').click();
+        
+        cy.get('@menu').select('Non-Canonical');
         cy.wait(500);
-        cy.url().should('include', 'include_non_canonical=false');
+        cy.url().should('include', 'canonical=false');
         cy.get('@tableRows').should('have.class', 'bg-status-success')
-        cy.get('@tableRows').should('not.have.class', 'bg-status-failed')
+        cy.get('@tableRows').should('not.have.class', 'bg-status-failed');
+
+        cy.get('@menu').select('All');
+        cy.wait(500);
+        cy.url().should('not.include', 'canonical');
+        cy.get('@tableRows').should('have.class', 'bg-status-success')
+        cy.get('@tableRows').should('have.class', 'bg-status-failed')
     }));
 
 })
