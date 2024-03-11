12e5bc458f851d2ba7577d00ea83d85c8b5dd828
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-11 15:25:07 -0300

    test canonical selection on the transaction table

--- cypress/e2e/canonicity/tables.cy.js
+++ cypress/e2e/canonicity/tables.cy.js
@@ -1,15 +1,28 @@
-suite(["@CI"],'blocks table', () => {
+suite(["@CI"],'table', () => {
 
-    let pages = ['/blocks', '/summary', '/'];
+    let pages = [{
+            url:'/transactions',
+            section: 'Transactions',
+        },
+        { 
+            url: '/blocks',
+            section: 'Blocks',
+        }, {
+            url: '/summary',
+            section: 'Blocks',
+        }, {
+            url: '/',
+            section: 'Blocks',
+        }, ];
 
-    pages.forEach(page => it(`on ${page} includes canonical blocks according to selection`, () => {
-        cy.visit(page);
+    pages.forEach(({url,section}) => it(`on ${url} includes canonical blocks according to selection`, () => {
+        cy.visit(url);
         cy.get('select#canonical-selection').as('menu');
         
         cy.get('@menu').select('Canonical');
         cy.url().should('include', 'canonical=true');
         cy.wait(500);
-        cy.contains('section','Blocks').find('table tr:not(:has(th)) span', {timeout: 60000}).as('tableRows')
+        cy.contains('section',section).find('table tr:not(:has(th)) span', {timeout: 60000}).as('tableRows')
         cy.get('@tableRows', {timeout: 20000}).should('not.have.class', 'bg-status-failed');
         cy.get('@tableRows', {timeout: 20000}).should('have.class', 'bg-status-success');
         
