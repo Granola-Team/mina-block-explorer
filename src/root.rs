37f4c2fd439875244d857e197ea7cb99f59336f5
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-12 10:07:32 -0300

    rename route component in root.rs file

--- src/root.rs
+++ src/root.rs
@@ -2,8 +2,8 @@ use crate::{
     account_dialog::dialog::AccountDialogView,
     addresses::page::{AccountSpotlightPage, AccountsPage, AddressesTabbedPage},
     blocks::page::{
-        BlockAnalyticsTab, BlockFeeTransfersTab, BlockSnarkJobsTab, BlockSpotlightTab, BlockTabbedPage,
-        BlockUserCommandsTab, LatestBlocksPage,
+        BlockAnalyticsTab, BlockInternalCommandsTab, BlockSnarkJobsTab, BlockSpotlightTab,
+        BlockTabbedPage, BlockUserCommandsTab, LatestBlocksPage,
     },
     broadcast::page::{
         BroadcastDelegationPage, BroadcastFromLedgerPage, BroadcastTransactionPage,
@@ -44,7 +44,7 @@ pub fn Root() -> impl IntoView {
                         <Route path="/spotlight" view=BlockSpotlightTab/>
                         <Route path="/user-commands" view=BlockUserCommandsTab/>
                         <Route path="/snark-jobs" view=BlockSnarkJobsTab/>
-                        <Route path="/fee-transfers" view=BlockFeeTransfersTab/>
+                        <Route path="/internal-commands" view=BlockInternalCommandsTab/>
                         <Route path="/analytics" view=BlockAnalyticsTab/>
                         <Route path="/*any" view=BlockSpotlightTab/>
                     </Route>
