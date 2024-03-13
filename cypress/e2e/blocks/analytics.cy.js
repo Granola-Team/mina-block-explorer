43238ba3501a1fae66ba19d3b5d2ac150ec7acdd
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-13 10:43:58 -0300

    fix tests

--- cypress/e2e/blocks/analytics.cy.js
+++ cypress/e2e/blocks/analytics.cy.js
@@ -2,7 +2,7 @@ suite(["@CI"],'block analytic tab', () => {
     it('contains the correct elements',() => {
         cy.visit('/blocks/3NKdghxnw7vQmVmj1G3MK1PQXYU5dDH1BQV3cCjXjViPW47L6hHJ/analytics');
         
-        cy.find('analytics-sm').should('have.lengthOf', 4);
-        cy.find('analytics-lg').should('have.lengthOf', 2);
+        cy.get('.analytics-sm').should('have.lengthOf', 4);
+        cy.get('.analytics-lg').should('have.lengthOf', 2);
     });
 });
