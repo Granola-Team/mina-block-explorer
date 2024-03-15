69075a8faa15ce860836c0701afeb25ca0f439a0
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-15 12:03:37 -0300

    remove test that is no longer relevant

--- cypress/e2e/navigation/to-transactions.cy.js
+++ cypress/e2e/navigation/to-transactions.cy.js
@@ -1,16 +1,3 @@
-suite(["@CI"],'transaction page', () => {
-    [{
-        origin: '/addresses/accounts/B62qq3TQ8AP7MFYPVtMx5tZGF3kWLJukfwG1A1RGvaBW1jfTPTkDBW6',
-        dest:"transactions",
-        href:"/transactions?account=B62qq3TQ8AP7MFYPVtMx5tZGF3kWLJukfwG1A1RGvaBW1jfTPTkDBW6"
-    }].forEach(({origin, dest, href}) => it(`is navigated to from ${dest}`,() => {
-        cy.visit(origin);
-        cy.get('a').contains("See all transactions", {timeout: 60000}).click();
-        cy.url().should('contain', href);
-    }));
-
-});
-
 suite(["@CI"],'transaction spotlight', () => {
     let pages = [
         { origin: '/transactions', column: 'Hash', tableHeader: 'Transactions'},
