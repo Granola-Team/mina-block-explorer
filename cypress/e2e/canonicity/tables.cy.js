dac19a2dcbc87bdbe4ff3b1b3d26832b9d35ce1d
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-24 08:23:45 -0300

    increase timeouts required to see table updates for canonical toggle

--- cypress/e2e/canonicity/tables.cy.js
+++ cypress/e2e/canonicity/tables.cy.js
@@ -25,22 +25,22 @@ suite(["@CI"],'table', () => {
 
         // should load canonical by default
         cy.contains('section',section).find('table tr:not(:has(th)) span', {timeout: 60000}).as('tableRows')
-        cy.get('@tableRows', {timeout: 20000}).should('not.have.class', 'bg-status-failed');
-        cy.get('@tableRows', {timeout: 20000}).should('have.class', 'bg-status-success');
+        cy.get('@tableRows', {timeout: 60000}).should('not.have.class', 'bg-status-failed');
+        cy.get('@tableRows', {timeout: 60000}).should('have.class', 'bg-status-success');
 
         cy.wait(500);
         cy.get('@menu').select('Non-Canonical');
         cy.wait(500);
         cy.url().should('include', 'canonical=false');
-        cy.get('@tableRows', {timeout: 20000}).should('not.have.class', 'bg-status-success');
-        cy.get('@tableRows', {timeout: 20000}).should('have.class', 'bg-status-failed');
+        cy.get('@tableRows', {timeout: 60000}).should('not.have.class', 'bg-status-success');
+        cy.get('@tableRows', {timeout: 60000}).should('have.class', 'bg-status-failed');
         
         cy.get('@menu').select('Canonical');
         cy.url().should('include', 'canonical=true');
         cy.wait(500);
         cy.contains('section',section).find('table tr:not(:has(th)) span', {timeout: 60000}).as('tableRows')
-        cy.get('@tableRows', {timeout: 20000}).should('not.have.class', 'bg-status-failed');
-        cy.get('@tableRows', {timeout: 20000}).should('have.class', 'bg-status-success');
+        cy.get('@tableRows', {timeout: 60000}).should('not.have.class', 'bg-status-failed');
+        cy.get('@tableRows', {timeout: 60000}).should('have.class', 'bg-status-success');
     }));
 
 })
