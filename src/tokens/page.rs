49c09d3114da3851df98d6f12f084997e3fe6f24
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-27 11:26:11 -0300

    just format

--- src/tokens/page.rs
+++ src/tokens/page.rs
@@ -1,5 +1,5 @@
-use crate::{common::{components::*, functions::*, table::*}};
 use super::functions::*;
+use crate::common::{components::*, functions::*, table::*};
 use leptos::*;
 use leptos_meta::*;
 
