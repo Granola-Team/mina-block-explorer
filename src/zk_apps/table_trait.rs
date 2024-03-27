d4e56de71ed29caebc35cace6fd3ce51b1da5c87
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-27 16:05:23 -0300

    add table trait to render the transaction data

--- src/zk_apps/table_trait.rs
+++ src/zk_apps/table_trait.rs
@@ -27,3 +27,46 @@ impl TableData for Vec<Option<ZkAppData>> {
             .collect::<Vec<_>>()
     }
 }
+
+impl TableData for Vec<Option<ZkAppTransactionData>> {
+    fn get_columns(&self) -> Vec<String> {
+        [
+            "Prover",
+            "Trx Hash",
+            "Age",
+            "Account Updates",
+            "Updated Accounts",
+            "Fee",
+        ]
+        .iter()
+        .map(ToString::to_string)
+        .collect::<Vec<_>>()
+    }
+
+    fn get_rows(&self) -> Vec<Vec<HtmlElement<html::AnyElement>>> {
+        self.iter()
+            .map(|opt_trx| match opt_trx {
+                Some(trx) => vec![
+                    convert_to_link(trx.prover.to_string(), "#".to_string()),
+                    convert_to_link(trx.hash.to_string(), "#".to_string()),
+                    convert_array_to_span(vec![
+                        convert_to_span(print_time_since(&trx.date_time.to_string())),
+                        convert_to_span(trx.date_time.to_string())
+                            .attr("class", "block text-xs font-light text-slate-400"),
+                    ])
+                    .attr("class", "block"),
+                    convert_to_pill(trx.updated_accounts.len().to_string(), ColorVariant::Blue),
+                    convert_array_to_span(
+                        trx.updated_accounts
+                            .iter()
+                            .map(|ua| convert_to_link(ua.to_string(), "#".to_string()))
+                            .collect::<Vec<_>>(),
+                    )
+                    .attr("class", "block"),
+                    convert_to_pill(trx.fee.to_string(), ColorVariant::Orange),
+                ],
+                None => vec![],
+            })
+            .collect::<Vec<_>>()
+    }
+}
