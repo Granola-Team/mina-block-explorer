520c087b0389568504f7cf7ce50cb144d9c223ce
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-03-29 12:36:39 -0300

    add a Zk App Details section

--- src/zk_apps/page.rs
+++ src/zk_apps/page.rs
@@ -3,6 +3,7 @@ use crate::{
     common::{components::*, functions::*, models::*, spotlight::*, table::*},
     icons::*,
 };
+use indoc::indoc;
 use leptos::*;
 use leptos_meta::*;
 
@@ -23,6 +24,16 @@ pub fn ZkAppSpotlight() -> impl IntoView {
                                     "mina".to_string(),
                                 ),
                                 ColorVariant::Green,
+                            ),
+                        ),
+                        ..Default::default()
+                    },
+                    SpotlightEntry {
+                        label: String::from("Total Txn"),
+                        any_el: Some(
+                            convert_to_pill(
+                                "52".to_string(),
+                                ColorVariant::Blue,
                             ),
                         ),
                         ..Default::default()
@@ -55,7 +66,105 @@ pub fn ZkAppSpotlight() -> impl IntoView {
             >
                 <ZKAppSymbol width=40/>
             </SpotlightSection>
+            <TableSection section_heading="ZK App Details".to_string() controls=|| ().into_view()>
+                <SpotlightTable>
+                    <ZkAppDetailTr>
+                        <ZkAppDetailTh>"Permissions :"</ZkAppDetailTh>
+                        <ZkAppDetailTd>
+                            <CodeBlock>
+
+                                {
+                                    indoc! {
+                                        r#"{
+    "access":"none"
+    "editActionState":"proof"
+    "editState":"proof"
+    "incrementNonce":"signature"
+    "receive":"none"
+    "send":"proof"
+    "setDelegate":"signature"
+    "setPermissions":"signature"
+    "setTiming":"signature"
+    "setTokenSymbol":"signature"
+    "setVerificationKey":"signature"
+    "setVotingFor":"signature"
+    "setZkAppUri":"signature"
+}"#
+                                    }
+                                }
+
+                            </CodeBlock>
+                        </ZkAppDetailTd>
+                    </ZkAppDetailTr>
+                    <ZkAppDetailTr>
+                        <ZkAppDetailTh>"Events :"</ZkAppDetailTh>
+                        <ZkAppDetailTd>
+                            <CodeBlock>
+
+                                {
+                                    indoc! {
+                                        r#"[
+    0:"25079927036070901246064867767436987657692091363973573142121686150614948079097"
+    1:"25079927036070901246064867767436987657692091363973573142121686150614948079097"
+    2:"25079927036070901246064867767436987657692091363973573142121686150614948079097"
+    3:"25079927036070901246064867767436987657692091363973573142121686150614948079097"
+    4:"25079927036070901246064867767436987657692091363973573142121686150614948079097"
+]"#
+                                    }
+                                }
+
+                            </CodeBlock>
+                        </ZkAppDetailTd>
+                    </ZkAppDetailTr>
+                    <ZkAppDetailTr>
+                        <ZkAppDetailTh>"App State :"</ZkAppDetailTh>
+                        <ZkAppDetailTd>
+                            <CodeBlock>
+
+                                {
+                                    indoc! {
+                                        r#"[
+    0:"1"
+    1:"0"
+    2:"0"
+    3:"0"
+    4:"0"
+    5:"0"
+    6:"0"
+    7:"0"
+]"#
+                                    }
+                                }
+
+                            </CodeBlock>
+                        </ZkAppDetailTd>
+                    </ZkAppDetailTr>
+                </SpotlightTable>
+            </TableSection>
         </PageContainer>
+    }
+}
+
+#[component]
+fn ZkAppDetailTd(children: Children) -> impl IntoView {
+    view! {
+        <td class="flex justify-start items-center m-1 p-1 text-left text-xs md:text-sm whitespace-nowrap">
+            {children()}
+        </td>
+    }
+}
+
+#[component]
+fn ZkAppDetailTr(children: Children) -> impl IntoView {
+    view! { <tr class="w-full flex flex-col lg:flex-row justify-start">{children()}</tr> }
+}
+
+#[component]
+fn ZkAppDetailTh(children: Children) -> impl IntoView {
+    view! {
+        <th class="flex justify-start items-start m-1 p-1 text-xs md:text-sm whitespace-nowrap w-36 md:w-40 min-w-36 md:min-w-40 font-normal text-slate-400">
+            {children()}
+        </th>
     }
 }
 
