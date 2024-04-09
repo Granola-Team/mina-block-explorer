41ad14f0c1f6c0612079f7c5e0aea75bdfeff16e
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-09 10:30:18 -0300

    improve legibility of table.rs code
    condense code
    
    just format
    
    just format and lint
    
    re-add the missing page class back in
    
    just format

--- src/common/table.rs
+++ src/common/table.rs
@@ -7,6 +7,13 @@ pub trait TableData {
     fn get_rows(&self) -> Vec<Vec<HtmlElement<AnyElement>>>;
 }
 
+const BUTTON_CLASS_BASE: &str = "font-semibold h-6 w-6 flex justify-center items-center";
+const PAGE_NUMBER_CLASS: &str =
+    "page text-md m-1 h-6 w-6 flex justify-center items-center font-semibold";
+const INACTIVE_PAGE_NUMBER_CLASS: &str =
+    "hover:bg-slate-300 bg-transparent rounded-full cursor-pointer";
+const CELL_PADDING_CLASS: &str = "first:pl-8 pl-2";
+
 #[component]
 pub fn Table<T>(data: T, #[prop(optional)] pagination: Option<Pagination>) -> impl IntoView
 where
@@ -14,142 +21,120 @@ where
 {
     let columns = data.get_columns();
     let rows = data.get_rows();
-    let cell_padding_class = "first:pl-8 pl-2";
-    let page_number_class = "text-md m-1 h-6 w-6 flex justify-center items-center font-semibold";
-    let inactive_page_number_class =
-        "cursor-pointer hover:bg-slate-300 bg-transparent rounded-full";
 
     view! {
         <div class="@container w-full overflow-auto">
             <table class="font-mono md:rounded-b-lg w-full @xs:w-[175%] @md:w-[150%] @2xl:w-[125%] @7xl:w-full">
-                <tr class="h-12 bg-table-header-fill">
-                    {columns
-                        .into_iter()
-                        .map(|s| {
-                            view! {
-                                <th class=format!(
-                                    "{} text-table-header-text-color font-semibold uppercase text-xs text-left",
-                                    cell_padding_class,
-                                )>{s}</th>
-                            }
-                        })
-                        .collect::<Vec<_>>()}
-                </tr>
-                {rows
-                    .into_iter()
-                    .map(|row| {
-                        view! {
-                            <tr class="h-12 bg-table-row-fill">
-
-                                {row
-                                    .iter()
-                                    .map(|cell| {
-                                        let cell_ellipsis_class = "text-ellipsis overflow-hidden";
-                                        let cell_class = format!(
-                                            "{} {} first:pl-8 pl-2 text-table-row-text-color font-medium text-sm text-left whitespace-nowrap max-w-40",
-                                            cell_padding_class,
-                                            cell_ellipsis_class,
-                                        );
-                                        view! {
-                                            <td class=cell_class>{cell.clone().into_view()}</td>
-                                        }
-                                    })
-                                    .collect::<Vec<_>>()}
-
-                            </tr>
-                        }
-                    })
-                    .collect::<Vec<_>>()}
+                {generate_table_header(&columns)} {generate_table_rows(&rows)}
             </table>
         </div>
-
-        {match pagination {
-            Some(pg) => {
-                let x_pages_around = x_surrounding_pages(pg.current_page, pg.total_pages());
-                let x_preceding_pages = &x_pages_around[0];
-                let x_following_pages = &x_pages_around[1];
-                view! {
-                    <div class="pagination-controls flex flex-col md:grid md:grid-cols-3 min-h-12 bg-table-header-fill">
-                        <span class="col-start-1 text-xs flex justify-center md:justify-start items-center font-bold pl-8 my-2">
-                            {format!(
-                                "Showing {} to {} of {} records",
-                                pg.start_index(),
-                                std::cmp::min(pg.end_index(), pg.total_records),
-                                pg.total_records,
-                            )}
-
-                        </span>
-                        <span class="button-container col-start-2 text-xs font-bold flex items-center justify-center my-2">
-                            <PaginationButton
-                                on_click=move |_| pg.set_current_page.update(|cp| *cp -= 1)
-                                disabled=x_preceding_pages.is_empty()
-                            >
-                                <ChevronLeft width=16/>
-                            </PaginationButton>
-                            {x_preceding_pages
-                                .iter()
-                                .map(|p| {
-                                    let p_inner = *p;
-                                    view! {
-                                        <button
-                                            on:click=move |_| {
-                                                pg.set_current_page.update(|cp| *cp = p_inner)
-                                            }
-
-                                            class=format!(
-                                                "page {} {}",
-                                                page_number_class,
-                                                inactive_page_number_class,
-                                            )
-                                        >
-
-                                            {p_inner}
-                                        </button>
-                                    }
-                                })
-                                .collect::<Vec<_>>()}
-
-                            <span class=format!(
-                                "page current-page text-white rounded-md bg-granola-orange {}",
-                                page_number_class,
-                            )>{pg.current_page}</span>
-                            {x_following_pages
-                                .iter()
-                                .map(|p| {
-                                    let p_inner = *p;
-                                    view! {
-                                        <button
-                                            on:click=move |_| {
-                                                pg.set_current_page.update(|cp| *cp = p_inner)
-                                            }
-
-                                            class=format!(
-                                                "page {} {}",
-                                                page_number_class,
-                                                inactive_page_number_class,
-                                            )
-                                        >
-
-                                            {p_inner}
-                                        </button>
-                                    }
-                                })
-                                .collect::<Vec<_>>()}
-
-                            <PaginationButton
-                                on_click=move |_| pg.set_current_page.update(|cp| *cp += 1)
-                                disabled=x_following_pages.is_empty()
-                            >
-                                <ChevronRight width=16/>
-                            </PaginationButton>
-                        </span>
-                    </div>
-                }
-                    .into_view()
-            }
-            None => ().into_view(),
-        }}
+        {generate_pagination(pagination)}
     }
+}
+
+fn generate_table_header(columns: &[String]) -> impl IntoView {
+    view! {
+        <tr class="h-12 bg-table-header-fill">
+            {columns
+                .iter()
+                .map(|s| {
+                    view! {
+                        <th class=format!(
+                            "{} text-table-header-text-color font-semibold uppercase text-xs text-left",
+                            CELL_PADDING_CLASS,
+                        )>{s}</th>
+                    }
+                })
+                .collect::<Vec<_>>()}
+        </tr>
+    }
+}
+
+fn generate_table_rows(rows: &[Vec<HtmlElement<AnyElement>>]) -> impl IntoView {
+    view! {
+        {rows
+            .iter()
+            .map(|row| {
+                view! {
+                    <tr class="h-12 bg-table-row-fill">
+                        {row
+                            .iter()
+                            .map(|cell| {
+                                let cell_ellipsis_class = "text-ellipsis overflow-hidden";
+                                let cell_class = format!(
+                                    "{} {} first:pl-8 pl-2 text-table-row-text-color font-medium text-sm text-left whitespace-nowrap max-w-40",
+                                    CELL_PADDING_CLASS,
+                                    cell_ellipsis_class,
+                                );
+                                view! { <td class=cell_class>{cell.clone().into_view()}</td> }
+                            })
+                            .collect::<Vec<_>>()}
+                    </tr>
+                }
+            })
+            .collect::<Vec<_>>()}
+    }
+}
+
+fn generate_pagination(pagination: Option<Pagination>) -> impl IntoView {
+    pagination.map(|pg| {
+        let x_pages_around = x_surrounding_pages(pg.current_page, pg.total_pages());
+        let create_page_button = |page_num: usize, current_page: usize| {
+            let is_current_page = page_num == current_page;
+            let button_classes = format!(
+                "{} {}", 
+                PAGE_NUMBER_CLASS,
+                if is_current_page { "current-page text-white rounded-md bg-granola-orange" } else { INACTIVE_PAGE_NUMBER_CLASS }
+            );
+
+            vec![view! {
+                <button
+                    class=button_classes
+                    on:click=move |_| if !is_current_page { pg.set_current_page.update(|cp| *cp = page_num) }
+                >{page_num}</button>
+            }.into_view()]
+        };
+
+        view! {
+            <div class="pagination-controls flex flex-col md:grid md:grid-cols-3 min-h-12 bg-table-header-fill">
+                <span class="col-start-1 text-xs flex justify-center md:justify-start items-center font-bold pl-8 my-2">
+                    {format!(
+                        "Showing {} to {} of {} records",
+                        pg.start_index(),
+                        std::cmp::min(pg.end_index(), pg.total_records),
+                        pg.total_records,
+                    )}
+
+                </span>
+                <span class="button-container col-start-2 text-xs font-bold flex items-center justify-center my-2">
+                    <PaginationButton
+                        on_click=move |_| {
+                            pg.set_current_page.update(|cp| *cp = pg.current_page.saturating_sub(1))
+                        }
+
+                        disabled=pg.current_page == 1
+                    >
+                        <ChevronLeft width=16/>
+                    </PaginationButton>
+                    {x_pages_around[0]
+                        .iter()
+                        .flat_map(|&p| create_page_button(p, pg.current_page))
+                        .collect::<Vec<_>>()}
+                    {create_page_button(pg.current_page, pg.current_page)}
+                    {x_pages_around[1]
+                        .iter()
+                        .flat_map(|&p| create_page_button(p, pg.current_page))
+                        .collect::<Vec<_>>()}
+                    <PaginationButton
+                        on_click=move |_| pg.set_current_page.update(|cp| *cp = pg.current_page + 1)
+                        disabled=pg.current_page == pg.total_pages()
+                    >
+                        <ChevronRight width=16/>
+                    </PaginationButton>
+                </span>
+            </div>
+        }.into_view()
+    }).unwrap_or_else(|| ().into_view())
 }
 
 #[component]
@@ -158,27 +143,24 @@ fn PaginationButton(
     #[prop(into)] on_click: Callback<MouseEvent>,
     disabled: bool,
 ) -> impl IntoView {
-    let button_class_base = "font-semibold h-6 w-6 flex justify-center items-center";
-    let button_class = match disabled {
-        true => format!(
-            "{} {}",
-            button_class_base, "text-slate-400 hover:cursor-not-allowed"
-        ),
-        false => format!(
-            "{} {}",
-            button_class_base, "hover:cursor-pointer hover:text-granola-orange hover:underline rounded-full bg-transparent hover:bg-slate-300"
-        ),
+    let button_class = if disabled {
+        format!(
+            "{} text-slate-400 hover:cursor-not-allowed",
+            BUTTON_CLASS_BASE
+        )
+    } else {
+        format!("{} hover:cursor-pointer hover:text-granola-orange hover:underline rounded-full bg-transparent hover:bg-slate-300", BUTTON_CLASS_BASE)
     };
+
     view! {
         <button
             class=button_class
             disabled=disabled
             type="button"
             on:click=move |event: MouseEvent| {
-                if disabled {
-                    return;
+                if !disabled {
+                    on_click.call(event)
                 }
-                on_click.call(event)
             }
         >
 
