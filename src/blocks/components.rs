e3df0e503104b54d013261aad5e6aad924496e2c
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-12 14:14:03 -0300

    make table height dynamic in SnarkJobs tab

--- src/blocks/components.rs
+++ src/blocks/components.rs
@@ -793,7 +793,8 @@ pub fn SummaryPageBlocksSection() -> impl IntoView {
 
 #[component]
 pub fn BlockSpotlightSnarkJobTable(block: BlocksQueryBlocks) -> impl IntoView {
-    let records_per_page = 10;
+    let page_dim = use_context::<ReadSignal<PageDimensions>>()
+        .expect("there to be a `PageDimensions` signal provided");
     let (current_page, set_current_page) = create_signal(1);
 
     view! {
@@ -807,15 +808,21 @@ pub fn BlockSpotlightSnarkJobTable(block: BlocksQueryBlocks) -> impl IntoView {
                         _ => {
                             let pag = build_pagination(
                                 snark_jobs.len(),
-                                records_per_page,
+                                TABLE_DEFAULT_PAGE_SIZE,
                                 current_page.get(),
                                 set_current_page,
-                                None,
-                                None,
+                                page_dim.get().height.map(|h| h as usize),
+                                Some(
+                                    Box::new(|container_height: usize| {
+                                        (container_height
+                                            - DEFAULT_ESTIMATED_NON_TABLE_SPACE_IN_SECTIONS)
+                                            / ESTIMATED_ROW_HEIGHT
+                                    }),
+                                ),
                             );
                             let subset = get_subset(
                                 &snark_jobs,
-                                records_per_page,
+                                pag.records_per_page,
                                 current_page.get() - 1,
                             );
                             view! { <Table data=subset pagination=pag/> }
