51350d3ea5195abebcfb056689922a5d94bc89c5
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-12 12:47:02 -0300

    refactor snarks page to leverage context in pagecontainer

--- src/snarks/page.rs
+++ src/snarks/page.rs
@@ -8,6 +8,17 @@ use leptos_router::{create_query_signal, use_query_map};
 
 #[component]
 pub fn SnarksPage() -> impl IntoView {
+    view! {
+        <Title text="SNARKs | Search For SNARKs"/>
+        <SearchBar placeholder="Exact search for prover"/>
+        <PageContainer>
+            <SnarksPageContents/>
+        </PageContainer>
+    }
+}
+
+#[component]
+fn SnarksPageContents() -> impl IntoView {
     let query_params_map = use_query_map();
     let (canonical_qp, _) = create_query_signal::<bool>("canonical");
 
@@ -24,49 +35,40 @@ pub fn SnarksPage() -> impl IntoView {
 
     let records_per_page = 10;
     let (current_page, set_current_page) = create_signal(1);
-
     view! {
-        <Title text="SNARKs | Search For SNARKs"/>
-        <SearchBar placeholder="Exact search for prover"/>
-        <PageContainer>
-            <TableSection
-                section_heading="SNARKs"
-                controls=move || {
-                    view! {
-                        <UrlParamSelectMenu
-                            id="canonical-selection"
-                            query_str_key="canonical"
-                            labels=UrlParamSelectOptions {
-                                is_boolean_option: true,
-                                cases: vec!["Canonical".to_string(), "Non-Canonical".to_string()],
-                            }
-                        />
-                    }
+        <TableSection
+            section_heading="SNARKs"
+            controls=move || {
+                view! {
+                    <UrlParamSelectMenu
+                        id="canonical-selection"
+                        query_str_key="canonical"
+                        labels=UrlParamSelectOptions {
+                            is_boolean_option: true,
+                            cases: vec!["Canonical".to_string(), "Non-Canonical".to_string()],
+                        }
+                    />
                 }
-            >
+            }
+        >
 
-                {move || match resource.get() {
-                    Some(Ok(data)) => {
-                        let pag = build_pagination(
-                            data.snarks.len(),
-                            records_per_page,
-                            current_page.get(),
-                            set_current_page,
-                            None,
-                            None,
-                        );
-                        let subset = get_subset(
-                            &data.snarks,
-                            records_per_page,
-                            current_page.get() - 1,
-                        );
-                        view! { <Table data=subset pagination=pag/> }
-                    }
-                    None => view! { <Table data=LoadingPlaceholder {}/> },
-                    _ => view! { <span></span> }.into_view(),
-                }}
+            {move || match resource.get() {
+                Some(Ok(data)) => {
+                    let pag = build_pagination(
+                        data.snarks.len(),
+                        records_per_page,
+                        current_page.get(),
+                        set_current_page,
+                        None,
+                        None,
+                    );
+                    let subset = get_subset(&data.snarks, records_per_page, current_page.get() - 1);
+                    view! { <Table data=subset pagination=pag/> }
+                }
+                None => view! { <Table data=LoadingPlaceholder {}/> },
+                _ => view! { <span></span> }.into_view(),
+            }}
 
-            </TableSection>
-        </PageContainer>
+        </TableSection>
     }
 }
