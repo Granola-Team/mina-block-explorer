001d83954d07ad0a94b754a5b97f07d984a43ee9
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-12 12:53:19 -0300

    refactor stakes page to leverage context in pagecontainer

--- src/stakes/page.rs
+++ src/stakes/page.rs
@@ -13,6 +13,29 @@ use leptos_router::*;
 #[component]
 pub fn StakesPage() -> impl IntoView {
     let (epoch_sig, _) = create_query_signal::<i64>("epoch");
+
+    view! {
+        <Title
+            text=move || {
+                if let Some(epoch) = epoch_sig.get() {
+                    format!("Epoch {}", epoch)
+                } else {
+                    "Current Staking Ledger".to_string()
+                }
+            }
+
+            formatter=move |text| format!("Staking Ledger | {text}")
+        />
+        <SearchBar placeholder="Exact search for public key"/>
+        <PageContainer>
+            <StakesPageContents/>
+        </PageContainer>
+    }
+}
+
+#[component]
+fn StakesPageContents() -> impl IntoView {
+    let (epoch_sig, _) = create_query_signal::<i64>("epoch");
     let (query_sig, _) = create_query_signal::<String>("query");
 
     let summary_resource = create_resource(|| (), |_| async move { load_summary_data().await });
@@ -38,94 +61,78 @@ pub fn StakesPage() -> impl IntoView {
 
     let records_per_page = 10;
     let (current_page, set_current_page) = create_signal(1);
-
     view! {
-        <Title
-            text=move || {
-                if let Some(epoch) = epoch_sig.get() {
-                    format!("Epoch {}", epoch)
-                } else {
-                    "Current Staking Ledger".to_string()
+        {move || match (resource.get(), summary_resource.get()) {
+            (Some(Ok(data)), Some(Ok(_))) => {
+                let (previous_epoch, next_epoch, curr_epoch, section_heading) = match (
+                    current_epoch(),
+                    epoch_sig.get(),
+                ) {
+                    (Some(curr_epoch), Some(qs_epoch)) => {
+                        let header = if curr_epoch == qs_epoch {
+                            "Current Staking Ledger".to_string()
+                        } else {
+                            format!("Epoch {} Staking Ledger", qs_epoch)
+                        };
+                        ((qs_epoch - 1), (qs_epoch + 1), (curr_epoch), header)
+                    }
+                    (Some(curr_epoch), None) => {
+                        (
+                            (curr_epoch - 1),
+                            (curr_epoch),
+                            (curr_epoch),
+                            "Current Staking Ledger".to_string(),
+                        )
+                    }
+                    _ => (0, 0, 0, "".to_string()),
+                };
+                let pag = build_pagination(
+                    data.stakes.len(),
+                    records_per_page,
+                    current_page.get(),
+                    set_current_page,
+                    None,
+                    None,
+                );
+                let subset = get_subset(&data.stakes, records_per_page, current_page.get() - 1);
+                view! {
+                    <TableSection
+                        section_heading=section_heading
+                        controls=move || {
+                            view! {
+                                <EpochButton
+                                    text="Previous"
+                                    style_variant=EpochStyleVariant::Secondary
+                                    epoch_target=previous_epoch
+                                />
+                                {if next_epoch == curr_epoch {
+                                    view! {
+                                        <StakesNavButton href="/next-stakes" text="Next Stakes"/>
+                                    }
+                                } else {
+                                    view! {
+                                        <EpochButton
+                                            text="Next"
+                                            style_variant=EpochStyleVariant::Primary
+                                            epoch_target=next_epoch
+                                        />
+                                    }
+                                }}
+                            }
+                        }
+                    >
+
+                        <Table data=subset pagination=pag/>
+                    </TableSection>
                 }
             }
-
-            formatter=move |text| format!("Staking Ledger | {text}")
-        />
-        <SearchBar placeholder="Exact search for public key"/>
-        <PageContainer>
-            {move || match (resource.get(), summary_resource.get()) {
-                (Some(Ok(data)), Some(Ok(_))) => {
-                    let (previous_epoch, next_epoch, curr_epoch, section_heading) = match (
-                        current_epoch(),
-                        epoch_sig.get(),
-                    ) {
-                        (Some(curr_epoch), Some(qs_epoch)) => {
-                            let header = if curr_epoch == qs_epoch {
-                                "Current Staking Ledger".to_string()
-                            } else {
-                                format!("Epoch {} Staking Ledger", qs_epoch)
-                            };
-                            ((qs_epoch - 1), (qs_epoch + 1), (curr_epoch), header)
-                        }
-                        (Some(curr_epoch), None) => {
-                            (
-                                (curr_epoch - 1),
-                                (curr_epoch),
-                                (curr_epoch),
-                                "Current Staking Ledger".to_string(),
-                            )
-                        }
-                        _ => (0, 0, 0, "".to_string()),
-                    };
-                    let pag = build_pagination(
-                        data.stakes.len(),
-                        records_per_page,
-                        current_page.get(),
-                        set_current_page,
-                        None,
-                        None,
-                    );
-                    let subset = get_subset(&data.stakes, records_per_page, current_page.get() - 1);
-                    view! {
-                        <TableSection
-                            section_heading=section_heading
-                            controls=move || {
-                                view! {
-                                    <EpochButton
-                                        text="Previous"
-                                        style_variant=EpochStyleVariant::Secondary
-                                        epoch_target=previous_epoch
-                                    />
-                                    {if next_epoch == curr_epoch {
-                                        view! {
-                                            <StakesNavButton href="/next-stakes" text="Next Stakes"/>
-                                        }
-                                    } else {
-                                        view! {
-                                            <EpochButton
-                                                text="Next"
-                                                style_variant=EpochStyleVariant::Primary
-                                                epoch_target=next_epoch
-                                            />
-                                        }
-                                    }}
-                                }
-                            }
-                        >
-
-                            <Table data=subset pagination=pag/>
-                        </TableSection>
-                    }
+            (_, _) => {
+                view! {
+                    <TableSection section_heading="" controls=move || ().into_view()>
+                        <Table data=LoadingPlaceholder {}/>
+                    </TableSection>
                 }
-                (_, _) => {
-                    view! {
-                        <TableSection section_heading="" controls=move || ().into_view()>
-                            <Table data=LoadingPlaceholder {}/>
-                        </TableSection>
-                    }
-                }
-            }}
-
-        </PageContainer>
+            }
+        }}
     }
 }
