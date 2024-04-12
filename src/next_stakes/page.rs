317e61278223948bc9157be6e2600706ce15e88e
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-12 14:05:35 -0300

    refactor next-stakes page to leverage context in pagecontainer

--- src/next_stakes/page.rs
+++ src/next_stakes/page.rs
@@ -9,6 +9,17 @@ use leptos_router::*;
 
 #[component]
 pub fn NextStakesPage() -> impl IntoView {
+    view! {
+        <Title text="Next Staking Ledger | Search For Stakers"/>
+        <SearchBar placeholder="Exact search for public key"/>
+        <PageContainer>
+            <NextStakesPageContents/>
+        </PageContainer>
+    }
+}
+
+#[component]
+fn NextStakesPageContents() -> impl IntoView {
     let query_params_map = use_query_map();
     let resource = create_resource(
         move || query_params_map.get(),
@@ -20,58 +31,53 @@ pub fn NextStakesPage() -> impl IntoView {
 
     let records_per_page = 10;
     let (current_page, set_current_page) = create_signal(1);
-
     view! {
-        <Title text="Next Staking Ledger | Search For Stakers"/>
-        <SearchBar placeholder="Exact search for public key"/>
-        <PageContainer>
-            <ErrorBoundary fallback=move |_| ().into_view()>
-                <Suspense fallback=move || {
-                    view! {
-                        <TableSection
-                            section_heading="Next Staking Ledger"
-                            controls=move || ().into_view()
-                        >
-                            <Table data=LoadingPlaceholder {}/>
-                        </TableSection>
-                    }
-                }>
-                    {move || {
-                        resource
-                            .get()
-                            .and_then(|res| res.ok())
-                            .map(|data| {
-                                let pag = build_pagination(
-                                    data.nextstakes.len(),
-                                    records_per_page,
-                                    current_page.get(),
-                                    set_current_page,
-                                    None,
-                                    None,
-                                );
-                                let subset = get_subset(
-                                    &data.nextstakes,
-                                    records_per_page,
-                                    current_page.get() - 1,
-                                );
-                                view! {
-                                    <TableSection
-                                        section_heading="Next Staking Ledger"
-                                        controls=move || {
-                                            view! {
-                                                <StakesNavButton href="/stakes" text="Current Stakes"/>
-                                            }
+        <ErrorBoundary fallback=move |_| ().into_view()>
+            <Suspense fallback=move || {
+                view! {
+                    <TableSection
+                        section_heading="Next Staking Ledger"
+                        controls=move || ().into_view()
+                    >
+                        <Table data=LoadingPlaceholder {}/>
+                    </TableSection>
+                }
+            }>
+                {move || {
+                    resource
+                        .get()
+                        .and_then(|res| res.ok())
+                        .map(|data| {
+                            let pag = build_pagination(
+                                data.nextstakes.len(),
+                                records_per_page,
+                                current_page.get(),
+                                set_current_page,
+                                None,
+                                None,
+                            );
+                            let subset = get_subset(
+                                &data.nextstakes,
+                                records_per_page,
+                                current_page.get() - 1,
+                            );
+                            view! {
+                                <TableSection
+                                    section_heading="Next Staking Ledger"
+                                    controls=move || {
+                                        view! {
+                                            <StakesNavButton href="/stakes" text="Current Stakes"/>
                                         }
-                                    >
+                                    }
+                                >
 
-                                        <Table data=subset pagination=pag/>
-                                    </TableSection>
-                                }
-                            })
-                    }}
+                                    <Table data=subset pagination=pag/>
+                                </TableSection>
+                            }
+                        })
+                }}
 
-                </Suspense>
-            </ErrorBoundary>
-        </PageContainer>
+            </Suspense>
+        </ErrorBoundary>
     }
 }
