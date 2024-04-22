e4e5a206bca13b8cab3ae00609ae5c3ce1b5f0b4
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-22 10:41:04 -0300

    add table component

---
+++ src/internal_commands/components.rs
@@ -0,0 +1,71 @@
+use crate::{
+    common::{components::*, constants::*, functions::*, models::PageDimensions, table::*},
+    internal_commands::{
+        functions::load_data, graphql::internal_commands_query::InternalCommandsQueryFeetransfers,
+    },
+};
+use leptos::*;
+use leptos_meta::Title;
+
+#[component]
+pub fn InternalCommands(
+    internal_commands: Vec<Option<InternalCommandsQueryFeetransfers>>,
+) -> impl IntoView {
+    view! {
+        <PageContainer>
+            <TableSection section_heading="Internal Commands" controls=|| ().into_view()>
+                <InternalCommandsTable internal_commands/>
+            </TableSection>
+        </PageContainer>
+    }
+}
+
+#[component]
+pub fn InternalCommandsTable(
+    internal_commands: Vec<Option<InternalCommandsQueryFeetransfers>>,
+) -> impl IntoView {
+    let page_dim = use_context::<ReadSignal<PageDimensions>>()
+        .expect("there to be a `PageDimensions` signal provided");
+    let (current_page, set_current_page) = create_signal(1);
+
+    view! {
+        {move || {
+            let pag = build_pagination(
+                internal_commands.len(),
+                TABLE_DEFAULT_PAGE_SIZE,
+                current_page.get(),
+                set_current_page,
+                page_dim.get().height.map(|h| h as usize),
+                Some(
+                    Box::new(|container_height: usize| {
+                        (container_height - DEFAULT_ESTIMATED_NON_TABLE_SPACE_IN_SECTIONS)
+                            / ESTIMATED_ROW_HEIGHT
+                    }),
+                ),
+            );
+            let subset = get_subset(
+                &internal_commands,
+                pag.records_per_page,
+                current_page.get() - 1,
+            );
+            view! { <Table data=subset pagination=pag/> }
+        }}
+    }
+}
+
+#[component]
+pub fn InternalCommandsTab() -> impl IntoView {
+    let resource = create_resource(|| (), |_| async move { load_data(TABLE_RECORD_SIZE).await });
+    view! {
+        <Title text="Transactions | Internal Commands"/>
+        {move || match resource.get() {
+            Some(Ok(data)) => view! { <InternalCommands internal_commands=data.feetransfers/> },
+            Some(Err(_)) => {
+                view! {
+                    <EmptyTable message="Unable to list internal commands at this time. Try refreshing."/>
+                }
+            }
+            None => view! { <EmptyTable message="No internal commands found"/> },
+        }}
+    }
+}
