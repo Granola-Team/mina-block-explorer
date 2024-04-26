77256da47a3bed031133c46290070b302f69b63a
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-26 09:01:53 -0300

    remove functionally useless visual regression suite

--- cypress/e2e/navigation/desktop-menu-look-and-feel.cy.js
+++
@@ -1,8 +0,0 @@
-const pages = require("../../pages.json");
-
-suite(["@VisualRegression"], "desktop", () => {
-  it(`has menu in header`, () => {
-    cy.visit("/summary");
-    cy.get("header").matchImageSnapshot(`desktop-header`);
-  });
-});
