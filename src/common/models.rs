d51f16958f4f599bbbff74b93776e40c516cf621
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-04-29 10:23:12 -0300

    remove multi-search

--- src/common/models.rs
+++ src/common/models.rs
@@ -175,8 +175,3 @@ pub struct PageDimensions {
     pub width: Option<f64>,
 }
 
-pub struct MultiSearch {
-    pub block_height: Option<i64>,
-    pub public_key: Option<String>,
-    pub state_hash: Option<String>,
-}
