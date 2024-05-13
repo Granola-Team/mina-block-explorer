2c1675a8e0b3afa502d6491759c5cddc79153bd8
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-13 14:40:50 -0300

    slow down the test by wiating on placeholders to appear and disappear

--- cypress/e2e/account/account-table.cy.js
+++ cypress/e2e/account/account-table.cy.js
@@ -3,6 +3,8 @@ import { DEFAULT_ACCOUNT_PK } from "../constants";
 suite(["@CI"], "account transaction table", () => {
   it("correctly references a counterparty", () => {
     cy.visit(`/addresses/accounts/${DEFAULT_ACCOUNT_PK}`);
+    cy.get(".loading-placeholder").should("exist");
+    cy.get(".loading-placeholder").should("not.exist");
     cy.aliasTableValue(0, "Counterparty", "User Commands", "counterparty");
 
     for (let i = 0; i <= 9; i++) {
