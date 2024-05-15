9271dcaaeeaffbd1fb7ffece949c4a15270d78e6
Jonathan Hult <Jonathan@JonathanHult.com>
2024-05-14 22:45:03 -0500

    style: show 0 instead of 0 MINA
    Granola-Team/mina-block-explorer#615

--- src/common/functions.rs
+++ src/common/functions.rs
@@ -182,9 +182,19 @@ pub fn decorate_with_currency_tag(
     data: String,
     currency_tag: String,
 ) -> HtmlElement<html::AnyElement> {
+    #![allow(unused_braces)]
     view! {
         <span class="whitespace-nowrap">
-            {data} <span class="ml-1 uppercase font-light text-inherit/50">{currency_tag}</span>
+            {if data != "0" {
+                view! {
+                    {data}
+                    <span class="ml-1 uppercase font-light text-inherit/50">{currency_tag}</span>
+                }
+                    .into_view()
+            } else {
+                view! { {data} }.into_view()
+            }}
+
         </span>
     }
     .into()
