89e02d3604f8302663632a3294dd84e757132071
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-20 14:27:57 -0300

    parity refactoring of internal commands table

--- src/internal_commands/components.rs
+++ src/internal_commands/components.rs
@@ -9,34 +9,6 @@ use leptos_router::create_query_signal;
 #[component]
 pub fn InternalCommandsTab() -> impl IntoView {
     let (metadata, _) = create_signal(Some(TableMetadata::default()));
-    view! {
-        <Title text="Transactions | Internal Commands"/>
-        <PageContainer>
-            <TableSection
-                metadata
-                section_heading="Internal Commands"
-                controls=move || {
-                    view! {
-                        <UrlParamSelectMenu
-                            id="canonical-selection"
-                            query_str_key="canonical"
-                            labels=UrlParamSelectOptions {
-                                is_boolean_option: true,
-                                cases: vec!["Canonical".to_string(), "Non-Canonical".to_string()],
-                            }
-                        />
-                    }
-                }
-            >
-
-                <InternalCommandsTable/>
-            </TableSection>
-        </PageContainer>
-    }
-}
-
-#[component]
-pub fn InternalCommandsTable() -> impl IntoView {
     let (recipient, _) = create_query_signal::<String>("q-recipient");
     let (height_sig, _) = create_query_signal::<i64>("q-height");
     let (state_hash_sig, _) = create_query_signal::<String>("q-state-hash");
@@ -83,33 +55,53 @@ pub fn InternalCommandsTable() -> impl IntoView {
     ];
     let table_cols_length = table_columns.len();
     let get_data = move || resource.get().and_then(|res| res.ok());
-
     view! {
-        <TableContainer>
-            <Table>
-                <TableHeader columns=table_columns/>
-                <Suspense fallback=move || {
+        <Title text="Transactions | Internal Commands"/>
+        <PageContainer>
+            <TableSection
+                metadata
+                section_heading="Internal Commands"
+                controls=move || {
                     view! {
-                        <TableRows data=vec![
-                            vec![LoadingPlaceholder; table_cols_length];
-                            TABLE_ROW_LIMIT.try_into().unwrap()
-                        ]/>
+                        <UrlParamSelectMenu
+                            id="canonical-selection"
+                            query_str_key="canonical"
+                            labels=UrlParamSelectOptions {
+                                is_boolean_option: true,
+                                cases: vec!["Canonical".to_string(), "Non-Canonical".to_string()],
+                            }
+                        />
                     }
-                }>
-                    {move || {
-                        get_data()
-                            .map(|data| {
-                                if data.feetransfers.is_empty() {
-                                    return view! {
-                                        <EmptyTable message="No internal commands found"/>
-                                    };
-                                }
-                                view! { <TableRows data=data.feetransfers/> }
-                            })
-                    }}
+                }
+            >
 
-                </Suspense>
-            </Table>
-        </TableContainer>
+                <TableContainer>
+                    <Table>
+                        <TableHeader columns=table_columns/>
+                        <Suspense fallback=move || {
+                            view! {
+                                <TableRows data=vec![
+                                    vec![LoadingPlaceholder; table_cols_length];
+                                    TABLE_ROW_LIMIT.try_into().unwrap()
+                                ]/>
+                            }
+                        }>
+                            {move || {
+                                get_data()
+                                    .map(|data| {
+                                        if data.feetransfers.is_empty() {
+                                            return view! {
+                                                <EmptyTable message="No internal commands found"/>
+                                            };
+                                        }
+                                        view! { <TableRows data=data.feetransfers/> }
+                                    })
+                            }}
+
+                        </Suspense>
+                    </Table>
+                </TableContainer>
+            </TableSection>
+        </PageContainer>
     }
 }
