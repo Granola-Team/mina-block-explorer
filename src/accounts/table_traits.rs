cab79f9a653a9db973d496e5e4ef1b09ea254392
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-22 08:43:10 -0300

    add table traits
    just format table_traits.rs

--- src/accounts/table_traits.rs
+++ src/accounts/table_traits.rs
@@ -1,40 +1,72 @@
-use super::models::AllAccountSummary;
-use crate::common::{functions::*, models::*, table::TableData};
+use crate::{
+    accounts::graphql::accounts_query::AccountsQueryAccounts,
+    common::{functions::*, models::*, table::TableData},
+};
 use leptos::*;
 
-impl TableData for Vec<Option<AllAccountSummary>> {
-    fn get_columns(&self) -> Vec<String> {
-        vec![
-            String::from("Public Key"),
-            String::from("Username"),
-            String::from("Balance"),
-            String::from("Nonce"),
-            String::from("Delegate"),
-            String::from("Time Locked"),
-        ]
-    }
-
+impl TableData for Vec<Option<AccountsQueryAccounts>> {
     fn get_rows(&self) -> Vec<Vec<HtmlElement<html::AnyElement>>> {
         self.iter()
-            .map(|all_acct_sum_opt| match all_acct_sum_opt {
-                Some(all_account_sum) => vec![
+            .map(|acct_opt| match acct_opt {
+                Some(account) => vec![
                     convert_to_link(
-                        all_account_sum.pk.to_string(),
-                        "/addresses/accounts/B62qr9zQ1LKnKM3d7wmFVjv3TzSV6fnXBZ162scK49NTHCA8Xc7PVKq".to_string(),
+                        account.get_public_key(),
+                        format!("/addresses/accounts/{}", account.get_public_key()),
                     ),
-                    convert_to_span(all_account_sum.username.to_string()),
-                        decorate_with_mina_tag(
-                            all_account_sum.balance.to_string()
-                        ),
-                    convert_to_pill(all_account_sum.nonce.to_string(), ColorVariant::Grey),
+                    convert_to_span(account.get_username()),
+                    decorate_with_mina_tag(account.get_balance()),
+                    convert_to_pill(account.get_nonce(), ColorVariant::Grey),
                     convert_to_link(
-                        all_account_sum.delegate.to_string(),
-                        "/addresses/accounts/B62qr9zQ1LKnKM3d7wmFVjv3TzSV6fnXBZ162scK49NTHCA8Xc7PVKq".to_string(),
+                        account.get_delegate(),
+                        format!("/addresses/accounts/{}", account.get_delegate()),
                     ),
-                    convert_to_span(false.to_string()),
+                    convert_to_span(account.get_timelocked().to_string()),
                 ],
                 None => vec![],
             })
             .collect::<Vec<_>>()
+    }
+}
+
+pub trait AccountTrait {
+    fn get_public_key(&self) -> String;
+    fn get_username(&self) -> String;
+    fn get_balance(&self) -> String;
+    fn get_nonce(&self) -> String;
+    fn get_delegate(&self) -> String;
+    fn get_timelocked(&self) -> bool;
+}
+
+impl AccountTrait for AccountsQueryAccounts {
+    fn get_public_key(&self) -> String {
+        self.public_key
+            .as_ref()
+            .cloned()
+            .unwrap_or_default()
+            .to_string()
+    }
+    fn get_username(&self) -> String {
+        self.username
+            .as_ref()
+            .cloned()
+            .unwrap_or_default()
+            .to_string()
+    }
+    fn get_balance(&self) -> String {
+        self.balance
+            .as_ref()
+            .cloned()
+            .map(|b| nanomina_to_mina(b as u64))
+            .unwrap_or_default()
+            .to_string()
+    }
+    fn get_nonce(&self) -> String {
+        self.nonce.unwrap_or_default().to_string()
+    }
+    fn get_delegate(&self) -> String {
+        self.delegate.as_ref().cloned().unwrap_or_default()
+    }
+    fn get_timelocked(&self) -> bool {
+        self.time_locked.unwrap_or(false)
     }
 }
