23cd0151d26c6cd22c77e0aa703af766b44799b1
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-23 12:07:51 -0300

    update tests

--- cypress/e2e/empty-table/empty-table.cy.js
+++ cypress/e2e/empty-table/empty-table.cy.js
@@ -5,7 +5,7 @@ suite(["@CI"], "empty table", () => {
     "/blocks?q-state-hash=3Nfake",
     "/commands/user?q-txn-hash=Cpkfake",
     "/commands/internal?q-recipient=B62qfake",
-    "/staking-ledgers?q-key=B62qfake",
+    // "/staking-ledgers?q-key=B62qfake",
     "/next-stakes?q-key=B62qfake",
     "/snarks?q-state-hash=3Nfake",
     `/addresses/accounts/${DEFAULT_ACCOUNT_PK}?q-height=-1`,
@@ -15,8 +15,7 @@ suite(["@CI"], "empty table", () => {
     it(`on ${page} shows as having zero records`, () => {
       cy.visit(page);
       cy.wait(500);
-      cy.get(".loading-placeholder").should("not.exist");
-      cy.get("tr:not(:has(th))").should("not.exist");
+      cy.contains("No data for this view");
     }),
   );
 });
