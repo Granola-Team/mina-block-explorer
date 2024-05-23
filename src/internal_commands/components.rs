7202bcdf95b244d8e43694b6c4b273a0b10a3ac6
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-23 08:56:05 -0300

    implement new view for internal commands

--- src/internal_commands/components.rs
+++ src/internal_commands/components.rs
@@ -1,5 +1,5 @@
 use crate::{
-    common::{components::*, constants::*, models::*, table::*},
+    common::{components::*, models::*, table::*},
     internal_commands::functions::load_data,
 };
 use leptos::*;
@@ -8,7 +8,7 @@ use leptos_router::create_query_signal;
 
 #[component]
 pub fn InternalCommandsTab() -> impl IntoView {
-    let (metadata, set_metadata) = create_signal(Some(TableMetadata::default()));
+    let (data_sig, set_data) = create_signal(None);
     let (recipient, _) = create_query_signal::<String>("q-recipient");
     let (height_sig, _) = create_query_signal::<i64>("q-height");
     let (state_hash_sig, _) = create_query_signal::<String>("q-state-hash");
@@ -26,6 +26,12 @@ pub fn InternalCommandsTab() -> impl IntoView {
             load_data(opt_recipient, height, state_hash, canonical).await
         },
     );
+    create_effect(move |_| {
+        resource
+            .get()
+            .and_then(|res| res.ok())
+            .map(|data| set_data.set(Some(data.feetransfers)))
+    });
 
     let table_columns = vec![
         TableColumn {
@@ -53,22 +59,14 @@ pub fn InternalCommandsTab() -> impl IntoView {
             is_searchable: false,
         },
     ];
-    let table_cols_length = table_columns.len();
-    let get_data = move || resource.get().and_then(|res| res.ok());
 
-    create_effect(move |_| {
-        if let Some(data) = get_data() {
-            set_metadata.set(Some(TableMetadata {
-                total_records: "all".to_string(),
-                displayed_records: data.feetransfers.len() as i64,
-            }))
-        }
-    });
     view! {
         <Title text="Transactions | Internal Commands"/>
         <PageContainer>
-            <TableSection
-                metadata
+            <TableSectionTemplate
+                table_columns
+                data_sig
+                is_loading=resource.loading()
                 section_heading="Internal Commands"
                 controls=move || {
                     view! {
@@ -82,35 +80,8 @@ pub fn InternalCommandsTab() -> impl IntoView {
                         />
                     }
                 }
-            >
+            />
 
-                <TableContainer>
-                    <Table>
-                        <TableHeader columns=table_columns/>
-                        <Suspense fallback=move || {
-                            view! {
-                                <TableRows data=vec![
-                                    vec![LoadingPlaceholder; table_cols_length];
-                                    TABLE_ROW_LIMIT.try_into().unwrap()
-                                ]/>
-                            }
-                        }>
-                            {move || {
-                                get_data()
-                                    .map(|data| {
-                                        if data.feetransfers.is_empty() {
-                                            return view! {
-                                                <EmptyTable message="No internal commands found"/>
-                                            };
-                                        }
-                                        view! { <TableRows data=data.feetransfers/> }
-                                    })
-                            }}
-
-                        </Suspense>
-                    </Table>
-                </TableContainer>
-            </TableSection>
         </PageContainer>
     }
 }
