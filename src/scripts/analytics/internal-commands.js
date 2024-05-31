ed8ccc1ff6c346ea52262f368a3fb1882fbff5c7
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-31 16:05:44 -0300

    Add scatter plot to fee transfer analytics to indicate totals per slot  (#729)
    * use global slot time for x axis
    * add scatter plot for fee transfers per slot group

--- src/scripts/analytics/internal-commands.js
+++ src/scripts/analytics/internal-commands.js
@@ -1,5 +1,6 @@
 setTimeout(async () => {
-  let blockLimit = 500;
+  const blockLimit = 500;
+  const groupSize = 10;
   let response = await fetch(config.graphql_endpoint_2, {
     method: "POST",
     headers: {
@@ -8,8 +9,14 @@ setTimeout(async () => {
     body: JSON.stringify({
       query: `query MyQuery() {
         feetransfers(query: { canonical: true }, sortBy: BLOCKHEIGHT_DESC, limit: ${blockLimit}) {
-          fee
-          dateTime
+          fee,
+          blockStateHash {
+            protocolState {
+              consensusState {
+                slotSinceGenesis
+              }
+            }
+          }
         }
       } 
     `,
@@ -18,15 +25,9 @@ setTimeout(async () => {
 
   let jsonResp = await response.json();
   let data = jsonResp.data.feetransfers.reduce((agg, record) => {
-    const date = new Date(record.dateTime);
-    const key =
-      date.getUTCFullYear() +
-      "-" +
-      String(date.getUTCMonth() + 1).padStart(2, "0") + // Month is zero-indexed, add one
-      "-" +
-      String(date.getUTCDate()).padStart(2, "0") +
-      " " +
-      String(date.getUTCHours()).padStart(2, "0");
+    let slot =
+      record.blockStateHash.protocolState.consensusState.slotSinceGenesis;
+    let key = slot - (slot % groupSize);
     let value = record.fee;
     if (!agg[key]) {
       agg[key] = [];
@@ -75,29 +76,46 @@ setTimeout(async () => {
     ],
     xAxis: {
       type: "category",
-      name: "Hour (UTC)",
-      axisLabel: {
-        rotate: 45,
-      },
+      name: "Global Slot",
       axisLabel: {
         formatter: function (value) {
           return xAxis[value];
         },
       },
     },
-    yAxis: {
-      type: "value",
-      name: "Fee",
-    },
+    yAxis: [
+      {
+        type: "value",
+        name: "Fee",
+        axisLabel: {
+          formatter: function (value) {
+            return `${value} Mina`;
+          },
+        },
+      },
+      {
+        type: "value",
+        name: "Transfers Count",
+        position: "right",
+        axisLabel: {
+          formatter: function (value) {
+            return `${value}`;
+          },
+        },
+      },
+    ],
     series: [
       {
         name: "boxplot",
         type: "boxplot",
         datasetIndex: 1,
+        yAxisIndex: 0,
         tooltip: {
           formatter: function (param) {
             return [
-              "Date: " + xAxis[param.name],
+              "Slot: " +
+                xAxis[param.name] +
+                `-${+xAxis[param.name] + groupSize - 1}`,
               "upper: " + param.data[5],
               "Q3: " + param.data[4],
               "median: " + param.data[3],
@@ -110,12 +128,29 @@ setTimeout(async () => {
       {
         name: "boxplot",
         type: "scatter",
+        symbolSize: 8,
         datasetIndex: 2,
+        yAxisIndex: 0,
         tooltip: {
           formatter: function (param) {
-            return ["Date: " + xAxis[param.name], "Fee: " + param.data[1]].join(
-              "<br/>",
-            );
+            return [
+              "Slot: " +
+                xAxis[param.name] +
+                `-${+xAxis[param.name] + groupSize - 1}`,
+              "Fee: " + param.data[1],
+            ].join("<br/>");
+          },
+        },
+      },
+      {
+        name: "transfers",
+        type: "scatter",
+        symbolSize: 12,
+        data: Object.values(data).map((fees, i) => ["" + i, fees.length]),
+        yAxisIndex: 1,
+        tooltip: {
+          formatter: function (param) {
+            return `Slot: ${xAxis[param.dataIndex]}-${+xAxis[param.dataIndex] + groupSize - 1}<br/>Transfers: ${param.value[1]}`;
           },
         },
       },
