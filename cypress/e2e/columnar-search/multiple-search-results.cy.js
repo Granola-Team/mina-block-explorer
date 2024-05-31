bca45f9c9d96c0223fe6a1a0204099c4515308b5
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-31 17:01:39 -0300

    fix test

--- cypress/e2e/columnar-search/multiple-search-results.cy.js
+++ cypress/e2e/columnar-search/multiple-search-results.cy.js
@@ -9,7 +9,7 @@ import { kebabCase } from "../helpers";
 
 let counterparty = "B62qjYanmV7y9njVeH5UHkz3GYBm7xKir1rAnoY4KsEYUGLMiU45FSM";
 let state_hash = "3NKxUy4mRpuH7MJxFQEobEJbUhPyvDyMEBQywmTRLbWsaHto3nur";
-let prover = "B62qs2Lw5WZNSjd8eHBUZXFYyRjV8oKtrZMFDn1S1Ye62G71xCQJMYM";
+let prover = "B62qpLeuZDL7PxNsCqsJwWFPAmnixi5ay8Kz9NcNGBQU8jK19VpJQaY";
 
 suite(["@CI"], "search with multiple results", () => {
   let multi_response_searches = [
