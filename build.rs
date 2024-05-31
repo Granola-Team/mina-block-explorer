05ce51bd1e558bf527c6cf13be74a39daab27d4c
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-05-31 20:37:28 -0300

    Connect to indexer REST url (#732)

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
     let graphql_url =
         env::var("GRAPHQL_URL").unwrap_or_else(|_| "https://graphql.minaexplorer.com".to_string());
     let rest_url =
-        env::var("REST_URL").unwrap_or_else(|_| "https://api.minaexplorer.com".to_string());
+        env::var("REST_URL").unwrap_or_else(|_| "https://api.minasearch.com".to_string());
     let commit_hash = env::var("VERSION").unwrap_or_else(|_| "version".to_string());
     let berkeley_feature_flag =
         env::var("BERKELEY_FEATURES_ENABLED").unwrap_or_else(|_| "false".to_string());
