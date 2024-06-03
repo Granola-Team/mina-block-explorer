046b46a097c90ada08ea9da06993f62e76586d9c
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-03 19:59:41 -0300

    make code more iditomatic

--- src/stakes/page.rs
+++ src/stakes/page.rs
@@ -9,6 +9,7 @@ pub fn StakesPage() -> impl IntoView {
     let (epoch_sig, _) = create_query_signal::<i64>("epoch");
     let summary_resource = create_resource(|| (), |_| async move { load_summary_data().await });
 
+    let get_data = move || summary_resource.get().and_then(|res| res.ok());
     view! {
         <Title
             text=move || {
@@ -22,26 +23,23 @@ pub fn StakesPage() -> impl IntoView {
             formatter=move |text| format!("Staking Ledger | {text}")
         />
         <PageContainer>
-            {move || match (summary_resource.get().and_then(|res| res.ok()), epoch_sig.get()) {
-                (Some(data), Some(selected_epoch)) => {
-                    view! {
-                        <StakesPageContents
-                            selected_epoch=Some(selected_epoch)
-                            current_epoch=data.epoch
-                            slot_in_epoch=data.slot
-                        />
+
+            {move || {
+                let current_epoch = get_data().map(|data| data.epoch);
+                let slot_in_epoch = get_data().map(|data| data.slot);
+                let selected_epoch = epoch_sig.get();
+                match (current_epoch, slot_in_epoch) {
+                    (Some(epoch), Some(slot)) => {
+                        view! {
+                            <StakesPageContents
+                                selected_epoch=selected_epoch
+                                current_epoch=epoch
+                                slot_in_epoch=slot
+                            />
+                        }
                     }
+                    _ => ().into_view(),
                 }
-                (Some(data), None) => {
-                    view! {
-                        <StakesPageContents
-                            selected_epoch=None
-                            current_epoch=data.epoch
-                            slot_in_epoch=data.slot
-                        />
-                    }
-                }
-                _ => ().into_view(),
             }}
 
         </PageContainer>
