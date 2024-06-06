c486a903c33420b751922729972022ba9c4b2493
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-06 09:13:39 -0300

    Ensure the memo is displaying in all relevant places

---
+++ cypress/e2e/user-commands/memo.cy.js
@@ -0,0 +1,17 @@
+const WHISPERIT_TXN_HASH =
+  "CkpZhvEYxb9YFZULWEw61khrHAqGZhTktZSiGQ1CKfe6n2TcnxdgD";
+suite(["@CI"], "user command memo field", () => {
+  // TODO: uncomment when filter fields working properly
+  // it('is present and decoded where appropriate', () => {
+  //     cy.visit(`/commands/user?q-txn-hash=${WHISPERIT_TXN_HASH}`);
+  //     cy.aliasTableRows("User Commands", "table-rows");
+  //     cy.get("@table-rows").should("have.length", 1);
+  //     cy.contains("whisperit#8145").should('exist');
+  //     cy.visit(`/addresses/accounts/B62qoaMj7u1JzuqXaBByQBL5jzqLguK8e7LHVPdY9LcvvLXK7HPsusD?q-txn-hash=${WHISPERIT_TXN_HASH}`);
+  //     cy.aliasTableRows("User Commands", "table-rows");
+  //     cy.get("@table-rows").should("have.length", 1);
+  //     cy.contains("whisperit#8145").should('exist');
+  //     cy.visit(`/commands/${WHISPERIT_TXN_HASH}`);
+  //     cy.contains("whisperit#8145").should('exist');
+  // });
+});
