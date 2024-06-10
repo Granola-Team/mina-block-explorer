a95d094b33972d5b20e0094a730408d5b8575cff
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-10 11:31:51 -0300

    enable stake delegation test

--- cypress/e2e/spotlight/transaction-spotlight.cy.js
+++ cypress/e2e/spotlight/transaction-spotlight.cy.js
@@ -2,6 +2,7 @@ const devices = require("../../devices.json");
 const {
   FIRST_TXN_HASH,
   FIRST_NON_CANONICAL_TXN_HASH,
+  STAKE_DELEGATION_HASH,
 } = require("../constants");
 
 suite(["@CI"], "transaction spotlight", () => {
@@ -36,20 +37,20 @@ suite(["@CI"], "transaction spotlight", () => {
     );
   });
 
-  // it("renders the tooltip for stake delegations", () => {
-  //   cy.visit(`/commands/${FIRST_TXN_HASH}`);
-  //   cy.get("section#spotlight-section table").within(() => {
-  //     cy.get("th").contains("Amount").as("amount");
-  //     cy.get("@amount").parent("tr").as("row");
-  //     cy.get("@row").within(() => {
-  //       cy.get("td .tooltip").should(
-  //         "have.attr",
-  //         "title",
-  //         "Stake delegations have no transacted amount",
-  //       );
-  //     });
-  //   });
-  // });
+  it("renders the tooltip for stake delegations", () => {
+    cy.visit(`/commands/${STAKE_DELEGATION_HASH}`);
+    cy.get("section#spotlight-section table").within(() => {
+      cy.get("th").contains("Amount").as("amount");
+      cy.get("@amount").parent("tr").as("row");
+      cy.get("@row").within(() => {
+        cy.get("td .tooltip").should(
+          "have.attr",
+          "title",
+          "Stake delegations have no transacted amount",
+        );
+      });
+    });
+  });
 
   it("does not render the tooltip for regular payments", () => {
     cy.visit(`/commands/${FIRST_TXN_HASH}`);
