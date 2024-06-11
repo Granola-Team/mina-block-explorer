0bafb9b8a281e67dd99f0737cb053cecaf6a2c40
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-11 09:05:39 -0300

    make summary label more clear

--- cypress/e2e/summary/overview.cy.js
+++ cypress/e2e/summary/overview.cy.js
@@ -1,16 +1,23 @@
 suite(["@CI"], "blockchain overview", () => {
-  it("displays unique block producers", () => {
+  it("displays non-zero metrics", () => {
     cy.visit("/blocks");
     cy.wait(1000);
-    cy.get("#uniqueBlockProducers .loading-placeholder").should("not.exist");
-    cy.get("#uniqueBlockProducers")
-      .invoke("text")
-      .then((text) => {
-        let value = parseInt(text);
-        expect(value).to.be.gt(0);
-      });
-    cy.get("#uniqueBlockProducers")
-      .siblings("label")
-      .should("have.text", "Block Producers in last 1000 Blocks");
+
+    let summaryItems = [
+      {
+        id: "#uniqueBlockProducers",
+        label: "Unique Producers of last 1000 Blocks",
+      },
+    ];
+    summaryItems.forEach(({ id, label }) => {
+      cy.get(`${id} .loading-placeholder`).should("not.exist");
+      cy.get(id)
+        .invoke("text")
+        .then((text) => {
+          let value = parseInt(text);
+          expect(value).to.be.gt(0);
+        });
+      cy.get(id).siblings("label").should("have.text", label);
+    });
   });
 });
