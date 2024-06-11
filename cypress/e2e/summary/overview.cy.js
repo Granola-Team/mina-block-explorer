a4c8eb3ddfe7f140ac234a57762447dcada2097e
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-11 10:26:12 -0300

    Update summary labels for Internal/User Commands (#814)
    * update summary labels
    
    * CI -> tier1
    
    * make cypress select section with headers more accurately
    
    * fix bug

--- cypress/e2e/summary/overview.cy.js
+++ cypress/e2e/summary/overview.cy.js
@@ -1,16 +1,25 @@
-suite(["@CI"], "blockchain overview", () => {
-  it("displays unique block producers", () => {
+suite(["@tier1"], "blockchain overview", () => {
+  it("displays non-zero metrics", () => {
     cy.visit("/blocks");
     cy.wait(1000);
-    cy.get("#uniqueBlockProducers .loading-placeholder").should("not.exist");
-    cy.get("#uniqueBlockProducers")
-      .invoke("text")
-      .then((text) => {
-        let value = parseInt(text);
-        expect(value).to.be.gt(0);
-      });
-    cy.get("#uniqueBlockProducers")
-      .siblings("label")
-      .should("have.text", "Block Producers in last 1000 Blocks");
+
+    let summaryItems = [
+      {
+        id: "#uniqueBlockProducers",
+        label: "Block Producers in last 1000 Blocks",
+      },
+      { id: "#totalUserCommands", label: "Total User Commands" },
+      { id: "#totalInternalCommands", label: "Total Internal Commands" },
+    ];
+    summaryItems.forEach(({ id, label }) => {
+      cy.get(`${id} .loading-placeholder`).should("not.exist");
+      cy.get(id)
+        .invoke("text")
+        .then((text) => {
+          let value = parseInt(text);
+          expect(value).to.be.gt(0);
+        });
+      cy.get(id).siblings("label").should("have.text", label);
+    });
   });
 });
