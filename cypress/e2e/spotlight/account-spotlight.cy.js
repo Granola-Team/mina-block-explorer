d1e43a5fcbdb2ff82beb580a8fd269d4d403925f
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-12 07:30:02 -0300

    test that columns contain spotlight address

--- cypress/e2e/spotlight/account-spotlight.cy.js
+++ cypress/e2e/spotlight/account-spotlight.cy.js
@@ -1,10 +1,24 @@
-import { DEFAULT_ACCOUNT_PK } from "../constants";
+import { ADDRESS_WITH_SNARK_AND_BLOCK_PRODUCTION } from "../constants";
 
 suite(["@tier1"], "Account spotlight", () => {
   let expected_fields = ["Balance", "Delegate"];
 
   it("displays complete information", () => {
-    cy.visit(`/addresses/accounts/${DEFAULT_ACCOUNT_PK}`);
-    cy.testSpotlight("Account Spotlight", DEFAULT_ACCOUNT_PK, expected_fields);
+    cy.visit(`/addresses/accounts/${ADDRESS_WITH_SNARK_AND_BLOCK_PRODUCTION}`);
+    cy.testSpotlight(
+      "Account Spotlight",
+      ADDRESS_WITH_SNARK_AND_BLOCK_PRODUCTION,
+      expected_fields,
+    );
+    cy.tableColumnValuesEqual(
+      "SNARK Jobs",
+      "Prover",
+      ADDRESS_WITH_SNARK_AND_BLOCK_PRODUCTION,
+    );
+    cy.tableColumnValuesEqual(
+      "Block Production",
+      "Block Producer",
+      ADDRESS_WITH_SNARK_AND_BLOCK_PRODUCTION,
+    );
   });
 });
