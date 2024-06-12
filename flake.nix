10dfdd10bd466eda774a0ee27cb1d18c216327b2
Robin Bate Boerop <me@robinbb.com>
2024-06-12 13:21:31 -0700

    Pin nixpkgs and use nixfmt

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
-  description = "A development environment and build system for the mina-block-explorer project";
+  description =
+    "A development environment and build system for the mina-block-explorer project";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url =
+      "github:NixOS/nixpkgs?ref=47b604b07d1e8146d5398b42d3306fdebd343986";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
-    flake-compat     = {
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
@@ -15,46 +17,37 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
+        pkgs = import nixpkgs { inherit system overlays; };
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
         # used to ensure rustfmt is nightly version to support unstable features
         nightlyToolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.minimal.override {
-            extensions = ["rustfmt"];
-          }
-        );
-      in
-      {
+          toolchain.minimal.override { extensions = [ "rustfmt" ]; });
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nightlyToolchain.passthru.availableComponents.rustfmt
-            cacert
-            cargo-audit
-            cargo-machete
-            cargo-nextest
-            leptosfmt
-            just
-            nodejs_18
-	    nodePackages.pnpm
-            openssl
-            pkg-config
-            rsync
-            rust-analyzer
-            toolchain
-            trunk
-            netcat
-          ] ++ lib.optionals stdenv.isDarwin
+          buildInputs = with pkgs;
             [
-              darwin.apple_sdk.frameworks.SystemConfiguration
-            ]
-          ;
+              nightlyToolchain.passthru.availableComponents.rustfmt
+              cacert
+              cargo-audit
+              cargo-machete
+              cargo-nextest
+              leptosfmt
+              just
+              nixfmt-classic
+              nodejs_18
+              nodePackages.pnpm
+              openssl
+              pkg-config
+              rsync
+              rust-analyzer
+              toolchain
+              trunk
+              netcat
+            ] ++ lib.optionals stdenv.isDarwin
+            [ darwin.apple_sdk.frameworks.SystemConfiguration ];
 
-          shellHook = ''
-            '';
+          shellHook = "";
         };
-      }
-    );
+      });
 }
