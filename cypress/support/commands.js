bb0950539da1ff685928666c405c6b0514009678
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-06-20 20:55:33 -0300

    move tests to tier1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -200,12 +200,12 @@ Cypress.Commands.add(
 );
 
 function extractMetadata(input) {
-  let regex = /Showing (\d+) of (\d+)/;
+  let regex = /Showing (\d+) of (\d+(?:,\d+)*)/;
   const match = input.match(regex);
   return match
     ? {
-        records: +match[1],
-        total_records: +match[2],
+        records: parseInt(match[1].replace(/,/g, ""), 10),
+        total_records: parseInt(match[2].replace(/,/g, ""), 10),
       }
     : null;
 }
