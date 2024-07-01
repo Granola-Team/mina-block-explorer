5942f18ac9e03d293282e5f959f5c595a0904cb4
Robin Bate Boerop <me@robinbb.com>
2024-06-30 18:27:26 -0700

    Footer changes

--- cypress/e2e/footer/visibility.cy.js
+++ cypress/e2e/footer/visibility.cy.js
@@ -2,18 +2,14 @@ suite(["@tier1"], "footer content", () => {
   let footer_links = [
     { text: "Granola", selector: 'a[href="https://granola.team"]' },
     {
-      text: "Docs",
+      text: "Disclaimer",
       selector:
-        'a[href="https://github.com/Granola-Team/mina-block-explorer/blob/main/DOCS/SITE_DOCS.md"]',
+        'a[href="https://gist.github.com/robinbb/05ba138b080ff5a95dcf8bb2d6ae76c5"]',
     },
     {
       text: "Terms",
       selector:
-        'a[href="https://gist.github.com/jhult/0a633e7771a695b0ffa529ab55722523"]',
-    },
-    {
-      text: "Support",
-      selector: 'a[href="https://docs.minaexplorer.com/minaexplorer/get-help"]',
+        'a[href="https://gist.github.com/robinbb/15b67f5d39dd47d37ddb88e3201dc311"]',
     },
   ];
 
