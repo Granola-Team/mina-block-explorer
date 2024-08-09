d806986e0161e182dcb3ee259fcae42b809dad60
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-08-09 10:33:05 -0300

    improve user experience

--- src/analytics/components.rs
+++ src/analytics/components.rs
@@ -1,31 +1,22 @@
 use super::{functions::*, models::*};
-use crate::common::table::{TableColumn, TableSectionTemplate};
+use crate::common::{
+    constants::DEFAULT_USER_INPUT_DEBOUNCE_INTERNVAL,
+    table::{TableColumn, TableSectionTemplate},
+};
 use leptos::*;
 use leptos_router::create_query_signal;
+use leptos_use::{use_debounce_fn_with_options, DebounceOptions};
 
 #[component]
 pub fn SnarkFees() -> impl IntoView {
+    let default_block_limit = 1000;
+    let input_element: NodeRef<html::Input> = create_node_ref();
     let (limit_sig, set_limit) = create_query_signal::<u64>("limit");
     let resource = create_resource(
         move || limit_sig.get(),
         move |limit| async move { load_snark_fees(limit).await },
     );
     let (data_sig, set_data) = create_signal(None);
-
-    let table_columns = vec![
-        TableColumn {
-            column: "Metric".to_string(),
-            ..Default::default()
-        },
-        TableColumn {
-            column: "All SNARKs".to_string(),
-            ..Default::default()
-        },
-        TableColumn {
-            column: "SNARKs with non-zero fees".to_string(),
-            ..Default::default()
-        },
-    ];
 
     create_effect(move |_| {
         set_data.set(Some(
@@ -36,37 +27,77 @@ pub fn SnarkFees() -> impl IntoView {
         ));
     });
 
-    view! {
-        <TableSectionTemplate
-            table_columns
-            data_sig
-            is_loading=resource.loading()
-            section_heading="SNARK Fees in latest blocks"
-            controls=move || {
-                view! {
-                    <input
-                        id="block-selection"
-                        type="number"
-                        on:input=move |ev| {
-                            set_limit.set(event_target_value(&ev).parse::<u64>().ok())
-                        }
+    create_effect(move |_| {
+        if limit_sig.get().is_none() {
+            set_limit.set(Some(default_block_limit));
+        }
+    });
 
-                        disabled=resource.loading()
-                        name="block-selection"
-                        step=200
-                        value=limit_sig.get()
-                        max=5000
-                        min=200
-                        class="block h-8 text-base text-sm font-normal font-mono p-2 text-right border rounded-sm border-slate-400 focus:border-granola-orange"
-                    />
-                    <label
-                        for="block-selection"
-                        class="flex items-center h-8 text-base text-sm font-normal font-mono p-2 whitespace-nowrap"
-                    >
-                        "latest blocks"
-                    </label>
-                }
+    let (handle_limit_sig, _) = create_signal(use_debounce_fn_with_options(
+        move || {
+            let v = input_element
+                .get()
+                .expect("<input/> should be mounted")
+                .value();
+            set_limit.set(v.parse::<u64>().ok());
+        },
+        DEFAULT_USER_INPUT_DEBOUNCE_INTERNVAL,
+        DebounceOptions::default(),
+    ));
+
+    {
+        move || {
+            let table_columns = vec![
+                TableColumn {
+                    column: "Metric".to_string(),
+                    ..Default::default()
+                },
+                TableColumn {
+                    column: "All SNARKs".to_string(),
+                    ..Default::default()
+                },
+                TableColumn {
+                    column: "SNARKs with non-zero fees".to_string(),
+                    ..Default::default()
+                },
+            ];
+            view! {
+                <TableSectionTemplate
+                    table_columns
+                    data_sig
+                    is_loading=resource.loading()
+                    section_heading=format!(
+                        "SNARK Fees in latest {} blocks",
+                        limit_sig.get().unwrap_or(default_block_limit),
+                    )
+
+                    controls=move || {
+                        view! {
+                            <input
+                                id="block-selection"
+                                type="number"
+                                on:keypress=move |ev| {
+                                    ev.prevent_default();
+                                }
+
+                                on:input=move |_| {
+                                    let handle_limit = handle_limit_sig.get_untracked();
+                                    handle_limit();
+                                }
+
+                                disabled=resource.loading()
+                                name="block-selection"
+                                step=1000
+                                value=limit_sig.get()
+                                max=5000
+                                min=1000
+                                class="block h-8 text-base text-sm font-normal font-mono p-2 text-right border rounded-sm border-slate-400 focus:border-granola-orange"
+                                node_ref=input_element
+                            />
+                        }
+                    }
+                />
             }
-        />
+        }
     }
 }
--- src/analytics/functions.rs
+++ src/analytics/functions.rs
@@ -2,9 +2,12 @@ use super::models::*;
 use crate::common::{constants::*, models::MyError};
 
 pub async fn load_snark_fees(limit: Option<u64>) -> Result<SnarkFeesResponse, MyError> {
+    if limit.is_none() {
+        return Err(MyError::ParseError("Limit must not be None".into()));
+    }
     let query_body = format!(
         r#"{{"query":"query SnarkFeesQuery(\n  $limit: Int = 100\n) {{\n  blocks(limit: $limit) {{\n    blockHeight\n    snarkFees\n    snarkJobs{{\n fee }}\n}}\n}}\n","variables":{{"limit": {}}},"operationName":"SnarkFeesQuery"}}"#,
-        limit.unwrap_or(100)
+        limit.unwrap()
     );
     let client = reqwest::Client::new();
     let response = client
