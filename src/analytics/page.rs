d8745cb8410b07799d3b5f7570f44a3e1a33844c
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-17 15:27:46 -0300

    add basic avg snark fee graph

--- src/analytics/page.rs
+++ src/analytics/page.rs
@@ -94,6 +94,18 @@ pub fn SnarksAnalyticsPage() -> impl IntoView {
     view! {
         <Title text="Analytics | SNARKs"/>
         <PageContainer>
+            <AppSection>
+                <AppHeading heading="Average SNARK fee per block in latest 10000 blocks"/>
+                <AnalyticsLayout>
+                    <AnalyticsXLContainer>
+                        <div id="avg-snark-fee" class="w-full h-96"></div>
+                        <script
+                            src="/scripts/analytics/avg-snark-fee-per-block.js"
+                            defer=true
+                        ></script>
+                    </AnalyticsXLContainer>
+                </AnalyticsLayout>
+            </AppSection>
             <SnarkFees/>
         </PageContainer>
     }
---
+++ src/scripts/analytics/avg-snark-fee-per-block.js
@@ -0,0 +1,119 @@
+setTimeout(async () => {
+  const blockLimit = 359604 - 500;
+
+  let chartDom = document.getElementById("avg-snark-fee");
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+  let myChart = echarts.init(chartDom);
+
+  myChart.showLoading({
+    text: "Loading...", // Display text with the spinner
+    color: "#E39844", // Spinner color
+    zlevel: 0,
+  });
+
+  let response = await fetch(config.graphql_endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `query SnarkFees($limit: Int = 10, $sort_by: SnarkSortByInput!, $query: SnarkQueryInput!) {
+        snarks(limit: $limit, sortBy: $sort_by, query: $query ) {
+          fee
+          blockHeight
+        }
+      }`,
+      variables: {
+        limit: 100000000, // very large limit to make block height the effective limit
+        sort_by: "BLOCKHEIGHT_DESC",
+        query: {
+          canonical: true,
+          blockHeight_gte: blockLimit,
+        },
+      },
+      operationName: "SnarkFees",
+    }),
+  });
+
+  let jsonResp = await response.json();
+  // Use reduce to aggregate transaction count and total amount per day
+  const data = jsonResp.data.snarks.reduce((acc, snark) => {
+    let key = snark.blockHeight;
+    if (!acc[key]) {
+      acc[key] = { count: 0, totalFees: 0 };
+    }
+
+    // Increment count and add to total amount
+    acc[key].count += 1;
+    acc[key].totalFees += snark.fee;
+
+    return acc;
+  }, {});
+
+  let heights = Object.keys(data).sort();
+  for (const height of heights) {
+    if (data[height].count != 0) {
+      data[height].totalFees = data[height].totalFees / 1e9; // nanomina to mina
+      data[height].avgFee = data[height].totalFees / data[height].count;
+    }
+  }
+  let counts = Object.values(data).map((e) => e.count);
+  let avgFees = Object.values(data).map((e) => e.avgFee);
+
+  let option;
+
+  myChart.hideLoading();
+
+  option = {
+    tooltip: {
+      position: "top",
+    },
+    title: {
+      text: `Avg fee by block, in last ${blockLimit} blocks`,
+      left: "center",
+    },
+    xAxis: {
+      type: "category",
+      data: heights,
+    },
+    yAxis: [
+      {
+        type: "value",
+        name: "Avg Fee Per Block",
+        axisLabel: {
+          formatter: (value) => `${value} MINA`,
+        },
+      },
+      {
+        type: "value",
+        name: "Fees Per Block",
+        position: "right",
+      },
+    ],
+    series: [
+      {
+        data: avgFees,
+        type: "scatter",
+        yAxisIndex: 0,
+        barMaxWidth: 20,
+        tooltip: {
+          valueFormatter: (value) => `${value.toFixed(5)} MINA`,
+        },
+      },
+      {
+        data: counts,
+        type: "scatter",
+        yAxisIndex: 1,
+        tooltip: {
+          valueFormatter: (value) => `${value} fees`,
+        },
+      },
+    ],
+  };
+
+  console.log(option);
+
+  option && myChart.setOption(option);
+}, 1000);
