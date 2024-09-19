de0efb1af82213f2b9ba30c4f81438f61c715455
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-19 17:34:55 -0300

    enable test

--- cypress/e2e/blocks/analytics.cy.js
+++ cypress/e2e/blocks/analytics.cy.js
@@ -1,10 +1,9 @@
 import { GENESIS_BLOCK_BLOCK_HASH } from "../constants";
 
 suite(["@tier2"], "block analytic tab", () => {
-  // TODO: too expensive to run this query for last 1000 blocks. Re-enable when not
-  // it("contains the correct elements", () => {
-  //   cy.visit(`/blocks/${GENESIS_BLOCK_BLOCK_HASH}/analytics`);
-  //   cy.get(".analytics-sm").should("have.lengthOf", 4);
-  //   cy.get(".analytics-lg").should("have.lengthOf", 2);
-  // });
+  it("contains the correct elements", () => {
+    cy.visit(`/blocks/${GENESIS_BLOCK_BLOCK_HASH}/analytics`);
+    cy.get(".analytics-sm").should("have.lengthOf", 4);
+    cy.get(".analytics-lg").should("have.lengthOf", 2);
+  });
 });
