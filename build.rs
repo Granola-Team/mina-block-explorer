131bc8f70e26be437c2bc417cd387ce76f94c130
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-20 13:28:07 -0300

    remove minasearch.com defaults

--- build.rs
+++ build.rs
@@ -1,10 +1,8 @@
 use std::env;
 
 fn main() {
-    let graphql_url = env::var("GRAPHQL_URL")
-        .unwrap_or_else(|_| "https://api.minasearch.com/graphql".to_string());
-    let rest_url =
-        env::var("REST_URL").unwrap_or_else(|_| "https://api.minasearch.com".to_string());
+    let graphql_url = env::var("GRAPHQL_URL").unwrap();
+    let rest_url = env::var("REST_URL").unwrap();
     let commit_hash = env::var("VERSION").unwrap_or_else(|_| "version".to_string());
 
     println!("cargo:rustc-env=GRAPHQL_URL={}", graphql_url);
