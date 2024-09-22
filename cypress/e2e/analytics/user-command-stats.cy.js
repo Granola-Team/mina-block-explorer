5eefed4d89f612d511543cc94b5e90d0cb1abc70
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-21 21:59:14 -0300

    fix test

--- cypress/e2e/analytics/user-command-stats.cy.js
+++ cypress/e2e/analytics/user-command-stats.cy.js
@@ -1,9 +1,11 @@
 suite(["@tier2"], "user commands stats", () => {
   it("are rendered", () => {
-    cy.visit("/analytics/user/commands?limit=10000");
-    cy.get("#total-transferred").should("have.value", "4.861 million MINA");
-    cy.get("#total-fees").should("have.value", "377.994 MINA");
-    cy.get("#total-number-of-transactions").should("have.value", "27,858");
-    cy.get("#total-failed-account-creations").should("have.value", "95");
+    cy.visit("/analytics/commands/user?limit=10000");
+    cy.get("#total-transferred").should("have.text", "4.861 million MINA", {
+      timeout: 15000,
+    });
+    cy.get("#total-fees").should("have.text", "377.994 MINA");
+    cy.get("#total-number-of-transactions").should("have.text", "27,858");
+    cy.get("#total-failed-account-creations").should("have.text", "95");
   });
 });
