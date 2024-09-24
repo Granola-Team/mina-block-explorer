42d3e2771d691883445053354971bd7b9eaa8a17
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-24 11:19:53 -0300

    add data loading function

--- src/analytics/functions.rs
+++ src/analytics/functions.rs
@@ -26,3 +26,54 @@ pub async fn load_snark_fees(limit: Option<u64>) -> Result<SnarkFeesResponse, My
         Err(MyError::NetworkError("Failed to fetch data".into()))
     }
 }
+
+pub async fn load_staker_leaderboard_data(
+    epoch: Option<u32>,
+    sort_by: StakerLeaderboardSort,
+) -> Result<StakerLeaderboardResponse, MyError> {
+    if epoch.is_none() {
+        return Err(MyError::ParseError("Epoch must not be None".into()));
+    }
+    let query_body = format!(
+        r#"{{"query":"query TopStakers($query: TopStakersQueryInput!, $limit: Int = 50, $sort_by: TopStakersSortByInput!) {{ topStakers(query: $query, limit: $limit, sortBy: $sort_by) {{ username public_key num_blocks_produced num_canonical_blocks_produced num_supercharged_blocks_produced num_slots_produced }} }}","variables":{{"limit": 50, "sort_by": {}, "query": {{ "epoch": {} }} }},"operationName":"TopStakers"}}"#,
+        sort_by,
+        epoch.unwrap()
+    );
+    let client = reqwest::Client::new();
+    let response = client
+        .post(GRAPHQL_ENDPOINT)
+        .body(query_body)
+        .send()
+        .await
+        .map_err(|e| MyError::NetworkError(e.to_string()))?;
+
+    if response.status().is_success() {
+        Ok(response
+            .json::<StakerLeaderboardResponse>()
+            .await
+            .map_err(|e| MyError::ParseError(e.to_string()))?)
+    } else {
+        Err(MyError::NetworkError("Failed to fetch data".into()))
+    }
+}
+
+pub async fn load_block_summary_data() -> Result<BlocksAnalyticsResponse, MyError> {
+    let query_body = r#"{"query":"query BlocksQuery(\n  $limit: Int = 1\n) {\n  blocks(limit: $limit) {\n    epoch_num_blocks\n    total_num_blocks\n  }\n}\n","variables":{"limit":1},"operationName":"BlocksQuery"}"#;
+    let client = reqwest::Client::new();
+    let response = client
+        .post(GRAPHQL_ENDPOINT)
+        .body(query_body)
+        .send()
+        .await
+        .map_err(|e| MyError::NetworkError(e.to_string()))?;
+
+    if response.status().is_success() {
+        let summary = response
+            .json::<BlocksAnalyticsResponse>()
+            .await
+            .map_err(|e| MyError::ParseError(e.to_string()))?;
+        Ok(summary)
+    } else {
+        Err(MyError::NetworkError("Failed to fetch data".into()))
+    }
+}
