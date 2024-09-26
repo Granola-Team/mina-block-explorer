76ab55698aeafd6c0dda6df81d9c7d414932b410
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-09-26 09:59:56 -0300

    augment default impl for StakeQueryInput

--- src/account_activity/models.rs
+++ src/account_activity/models.rs
@@ -208,6 +208,7 @@ impl AccountActivityQueryDirectionalTransactionTrait
 impl Default for StakeQueryInput {
     fn default() -> Self {
         StakeQueryInput {
+            stake_lte: None,
             timing_exists: None,
             chain_id_gte: None,
             balance_ne: None,
