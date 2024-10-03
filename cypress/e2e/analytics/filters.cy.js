e061284f0b30ee72e957d9dd1240395af041468d
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-10-03 07:00:57 -0300

    update tests

--- cypress/e2e/analytics/filters.cy.js
+++ cypress/e2e/analytics/filters.cy.js
@@ -2,34 +2,39 @@ suite(["@tier2"], "fitler", () => {
   let pages = [
     {
       url: "/analytics/commands/user",
-      filter_id: "#block-limit",
-      expected_url_value: 1000,
-      expected_url_key: "limit",
+      filter_ids: ["#blockheight-gte", "#blockheight-lte"],
+      expected_urls_values: [7000, 10000],
+      expected_url_keys: ["q-blockheight-gte", "q-blockheight-lte"],
     },
     {
       url: "/analytics/snarks",
-      filter_id: "#block-limit",
-      expected_url_value: 1000,
-      expected_url_key: "limit",
+      filter_ids: ["#blockheight-gte", "#blockheight-lte"],
+      expected_urls_values: [7000, 10000],
+      expected_url_keys: ["q-blockheight-gte", "q-blockheight-lte"],
     },
     {
       url: "/analytics/staker-leaderboard",
-      filter_id: "#epoch",
-      expected_url_value: 0,
-      expected_url_key: "epoch",
+      filter_ids: ["#epoch"],
+      expected_url_values: [0],
+      expected_url_keys: ["epoch"],
     },
     {
       url: "/analytics/snarker-leaderboard",
-      filter_id: "#epoch",
-      expected_url_value: 0,
-      expected_url_key: "epoch",
+      filter_ids: ["#epoch"],
+      expected_url_values: [0],
+      expected_url_keys: ["epoch"],
     },
   ];
-  pages.forEach(({ url, filter_id, expected_url_value, expected_url_key }) =>
+  pages.forEach(({ url, filter_ids, expected_url_values, expected_url_keys }) =>
     it(`has defaults for ${url}`, () => {
       cy.visit(url);
-      cy.get(filter_id).should("have.value", expected_url_value);
-      cy.url().should("include", `${expected_url_key}=${expected_url_value}`);
+      filter_ids.forEach((filter_id, index) => {
+        const expected_url_value = expected_url_values[index];
+        const expected_url_key = expected_url_keys[index];
+
+        cy.get(filter_id).should("have.value", expected_url_value);
+        cy.url().should("include", `${expected_url_key}=${expected_url_value}`);
+      });
     }),
   );
 });
