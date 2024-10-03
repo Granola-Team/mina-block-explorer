2d0618dde04ca7a07eb068b27e95034d17fe2bd2
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-10-03 07:09:38 -0300

    update tests

--- cypress/e2e/analytics/filters.cy.js
+++ cypress/e2e/analytics/filters.cy.js
@@ -1,15 +1,21 @@
 suite(["@tier2"], "fitler", () => {
   let pages = [
     {
-      url: "/analytics/commands/user",
+      url: "/analytics/commands/user?q-blockheight-gte=0&q-blockheight-lte=10000",
       filter_ids: ["#blockheight-gte", "#blockheight-lte"],
-      expected_urls_values: [7000, 10000],
+      expected_url_values: [0, 10000],
       expected_url_keys: ["q-blockheight-gte", "q-blockheight-lte"],
     },
     {
-      url: "/analytics/snarks",
+      url: "/analytics/commands/user?q-blockheight-gte=0&q-blockheight-lte=10000",
       filter_ids: ["#blockheight-gte", "#blockheight-lte"],
-      expected_urls_values: [7000, 10000],
+      expected_url_values: [0, 10000],
+      expected_url_keys: ["q-blockheight-gte", "q-blockheight-lte"],
+    },
+    {
+      url: "/analytics/snarks?q-blockheight-gte=0&q-blockheight-lte=10000",
+      filter_ids: ["#blockheight-gte", "#blockheight-lte"],
+      expected_url_values: [0, 10000],
       expected_url_keys: ["q-blockheight-gte", "q-blockheight-lte"],
     },
     {
@@ -29,8 +35,8 @@ suite(["@tier2"], "fitler", () => {
     it(`has defaults for ${url}`, () => {
       cy.visit(url);
       filter_ids.forEach((filter_id, index) => {
-        const expected_url_value = expected_url_values[index];
-        const expected_url_key = expected_url_keys[index];
+        const expected_url_value = expected_url_values[+index];
+        const expected_url_key = expected_url_keys[+index];
 
         cy.get(filter_id).should("have.value", expected_url_value);
         cy.url().should("include", `${expected_url_key}=${expected_url_value}`);
