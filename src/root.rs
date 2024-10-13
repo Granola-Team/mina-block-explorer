4a6ef3b87a45197b65dcc800b35d5073bac34ffc
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-10-13 16:54:19 -0300

    fix broken back-navigation on redirections

--- src/root.rs
+++ src/root.rs
@@ -51,10 +51,36 @@ pub fn Root() -> impl IntoView {
             <main>
                 <Routes>
                     // redirect any non-existent URL back to the blocks page
-                    <Route path="/*" view=move || view! { <Redirect path="/blocks" /> } />
+                    <Route
+                        path="/*"
+                        view=move || {
+                            view! {
+                                <Redirect
+                                    path="/blocks"
+                                    options=NavigateOptions {
+                                        replace: true,
+                                        ..Default::default()
+                                    }
+                                />
+                            }
+                        }
+                    />
                     <Route path="/addresses/accounts" view=AccountsPage />
                     <Route path="/addresses/accounts/:id" view=AccountSpotlightTabbedPage>
-                        <Route path="*" view=move || view! { <Redirect path="commands/user" /> } />
+                        <Route
+                            path="*"
+                            view=move || {
+                                view! {
+                                    <Redirect
+                                        path="commands/user"
+                                        options=NavigateOptions {
+                                            replace: true,
+                                            ..Default::default()
+                                        }
+                                    />
+                                }
+                            }
+                        />
                         <Route path="/commands/user" view=AccountUserCommandsPage />
                         <Route path="/commands/internal" view=AccountInternalCommandsPage />
                         <Route path="/snark-jobs" view=AccountSnarkWorkPage />
@@ -67,7 +93,20 @@ pub fn Root() -> impl IntoView {
 
                     <Route path="/blocks" view=SummaryPage />
                     <Route path="/blocks/:id" view=BlockTabbedPage>
-                        <Route path="/*" view=move || view! { <Redirect path="spotlight" /> } />
+                        <Route
+                            path="/*"
+                            view=move || {
+                                view! {
+                                    <Redirect
+                                        path="spotlight"
+                                        options=NavigateOptions {
+                                            replace: true,
+                                            ..Default::default()
+                                        }
+                                    />
+                                }
+                            }
+                        />
                         <Route path="/spotlight" view=BlockSpotlightTab />
                         <Route path="/snark-jobs" view=BlockSnarkJobsTab />
                         <Route path="/commands/user" view=BlockUserCommandsTab />
@@ -76,7 +115,20 @@ pub fn Root() -> impl IntoView {
                     </Route>
 
                     <Route path="/commands" view=CommandsTabbedPage>
-                        <Route path="*" view=move || view! { <Redirect path="user" /> } />
+                        <Route
+                            path="*"
+                            view=move || {
+                                view! {
+                                    <Redirect
+                                        path="user"
+                                        options=NavigateOptions {
+                                            replace: true,
+                                            ..Default::default()
+                                        }
+                                    />
+                                }
+                            }
+                        />
                         <Route path="/user" view=UserCommandsPage />
                         <Route path="/internal" view=InternalCommandsTab />
                         <Route path="/zk-app" view=ZkAppTransactionsPage />
@@ -88,13 +140,39 @@ pub fn Root() -> impl IntoView {
                     <Route path="/staking-ledgers" view=StakesPage />
 
                     <Route path="/broadcast" view=DelegationTabbedPage>
-                        <Route path="/*" view=move || view! { <Redirect path="transaction" /> } />
+                        <Route
+                            path="/*"
+                            view=move || {
+                                view! {
+                                    <Redirect
+                                        path="transaction"
+                                        options=NavigateOptions {
+                                            replace: true,
+                                            ..Default::default()
+                                        }
+                                    />
+                                }
+                            }
+                        />
                         <Route path="/transaction" view=BroadcastTransactionPage />
                         <Route path="/delegation" view=BroadcastDelegationPage />
                         <Route path="/ledger" view=BroadcastFromLedgerPage />
                     </Route>
                     <Route path="/analytics" view=AnalyticsTabbedPage>
-                        <Route path="*" view=move || view! { <Redirect path="blocks" /> } />
+                        <Route
+                            path="*"
+                            view=move || {
+                                view! {
+                                    <Redirect
+                                        path="blocks"
+                                        options=NavigateOptions {
+                                            replace: true,
+                                            ..Default::default()
+                                        }
+                                    />
+                                }
+                            }
+                        />
                         <Route path="/commands/user" view=UserCommandsAnalyticsPage />
                         <Route path="/blocks" view=BlocksAnalyticsPage />
                         <Route path="/snarks" view=SnarksAnalyticsPage />
