826ac5caffc7bbb06593fb26ce03ebb26d1c6ed4
Nathan Tranquilla <n1tranquilla@gmail.com>
2024-10-30 13:47:08 -0300

    remove redundant closure

--- src/stakes/functions.rs
+++ src/stakes/functions.rs
@@ -65,7 +65,7 @@ pub fn get_balance(stake: &StakingLedgersQueryStakes) -> String {
     stake
         .balance
         .map(|number| format!("{:.1}", number))
-        .map(|number| format_mina(number))
+        .map(format_mina)
         .map(|number| format_number_for_html(&number, 11))
         .unwrap_or_default()
 }
