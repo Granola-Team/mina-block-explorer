d897818171075c616060ff33b473b356167078b4
Jonathan Hult <Jonathan@JonathanHult.com>
2025-02-11 22:27:50 -0600

    build: use default values to ensure Rust Analyzer works properly in Zed

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 use std::env;
 
 fn main() {
-    let graphql_url = env::var("GRAPHQL_URL").unwrap();
-    let rest_url = env::var("REST_URL").unwrap();
+    let graphql_url =
+        env::var("GRAPHQL_URL").unwrap_or("https://api.minasearch.com/graphql".into());
+    let rest_url = env::var("REST_URL").unwrap_or("https://api.minasearch.com".into());
     let commit_hash = env::var("VERSION").unwrap_or_else(|_| "version".to_string());
 
     println!("cargo:rustc-env=GRAPHQL_URL={}", graphql_url);
