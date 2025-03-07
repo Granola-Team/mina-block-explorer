0605be13b7cb4ffb181df063e8c1e0fa22bd9213
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-03-07 14:11:08 -0400

    refactor to use tokens endpoint, not tokenholders

--- src/tokens/graphql.rs
+++ src/tokens/graphql.rs
@@ -3,8 +3,8 @@ use graphql_client::GraphQLQuery;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/schemas/mina-explorer.graphql",
-    query_path = "graphql/queries/token_holders.graphql",
+    query_path = "graphql/queries/tokens.graphql",
     response_derives = "Serialize,PartialEq,Debug,Clone,Default",
     skip_serializing_none
 )]
-pub struct TokenHoldersQuery;
+pub struct TokensQuery;
