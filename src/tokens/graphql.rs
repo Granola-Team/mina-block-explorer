5f7dbd6bbb7cbd30709920698509b162279a0c46
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-03-11 16:13:25 -0300

    Revert "refactor to use tokens endpoint, not tokenholders"
    This reverts commit 0605be13b7cb4ffb181df063e8c1e0fa22bd9213.

--- src/tokens/graphql.rs
+++ src/tokens/graphql.rs
@@ -3,8 +3,8 @@ use graphql_client::GraphQLQuery;
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "graphql/schemas/mina-explorer.graphql",
-    query_path = "graphql/queries/tokens.graphql",
+    query_path = "graphql/queries/token_holders.graphql",
     response_derives = "Serialize,PartialEq,Debug,Clone,Default",
     skip_serializing_none
 )]
-pub struct TokensQuery;
+pub struct TokenHoldersQuery;
