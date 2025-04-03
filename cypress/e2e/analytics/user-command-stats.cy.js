9e40e08ad748b73f62ea24d9dbd8edfc57fe6fa8
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-03 09:39:19 -0300

    make testing user command stats simpler

--- cypress/e2e/analytics/user-command-stats.cy.js
+++ cypress/e2e/analytics/user-command-stats.cy.js
@@ -3,11 +3,8 @@ suite(["@tier2"], "user commands stats", () => {
     cy.visit(
       "/analytics/commands/user?q-blockheight-gte=0&q-blockheight-lte=10000",
     );
-    cy.get("#total-transferred").should("have.text", "4.861", {
-      timeout: 15000,
-    });
-    cy.get("#total-fees").should("have.text", "378.031");
-    cy.get("#total-number-of-transactions").should("have.text", "27,868");
-    cy.get("#total-failed-account-creations").should("have.text", "95");
+    cy.getBySel("analytics-simple-info").each(($el) =>
+      cy.wrap($el).checkNumeric(),
+    );
   });
 });
