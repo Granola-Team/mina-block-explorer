b0b68bffbaa7a05ce0d4a4ae2be22bd510ad4692
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-05 11:46:33 -0300

    add a table test for zk app spotlight, accounts updated table

---
+++ cypress/e2e/tables/tests/table.zkapp-txn-spotlight.cy.js
@@ -0,0 +1,4 @@
+// Import the main runTestSuite function and the specific test suite data
+import { runTestSuite } from "../run-test-suite";
+import testSuiteData from "../data/data.zkapp-txn-spotlight.js";
+runTestSuite(testSuiteData);
