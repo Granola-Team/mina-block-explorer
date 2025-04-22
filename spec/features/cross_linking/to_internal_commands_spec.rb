62c77f8e0f8ba1d5869fd9035009de19475ad53e
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-22 11:09:53 -0300

    move internal txn cross linking over to ruby

---
+++ spec/features/cross_linking/to_internal_commands_spec.rb
@@ -0,0 +1,32 @@
+require "spec_helper"
+
+RSpec.describe "Internal commands page navigation", type: :system do
+  pages = [
+    {
+      origin: "/addresses/accounts/#{Constants::GENESIS_ACCOUNT_PK}/commands/internal",
+      dest: "internal commands",
+      href: "/commands/internal?q-recipient=#{Constants::GENESIS_ACCOUNT_PK}"
+    }
+  ]
+
+  pages.each do |test_case|
+    it "is navigated to from #{test_case[:origin]}" do
+      visit test_case[:origin]
+
+      # Click the link with text "See all internal commands"
+      find("a", text: "SEE ALL INTERNAL COMMANDS").click
+
+      # Verify the URL contains the expected href
+      expect(page.current_url).to include(test_case[:href]), "Expected URL to include '#{test_case[:href]}', but was #{page.current_url}"
+
+      # Verify the "Recipient" column in the "Internal Commands" table contains GENESIS_ACCOUNT_PK
+      wait_until_table_loaded("Internal Commands")
+      recipient_cells = all(table_column_selector("Internal Commands", "Recipient".upcase))
+      recipient_cells.each do |cell|
+        # Join split text if necessary (as per your previous issue)
+        cleaned_text = cell.text.gsub(/[\n+-]/, "")
+        expect(cleaned_text).to eq(Constants::GENESIS_ACCOUNT_PK), "Expected 'Recipient' column to contain '#{Constants::GENESIS_ACCOUNT_PK}', but found '#{cleaned_text}'"
+      end
+    end
+  end
+end
