440a9319a478c71c82832edf0d83d16c382c7e73
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-22 13:29:19 -0300

    move stakes cross linking test to ruby

---
+++ spec/features/cross_linking/to_stakes_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+RSpec.describe "Staking ledger page navigation", type: :system do
+  pages = [
+    {
+      origin: "/addresses/accounts/#{Constants::GENESIS_ACCOUNT_PK}/delegations",
+      dest: "staking-ledgers",
+      href: "q-delegate=#{Constants::GENESIS_ACCOUNT_PK}"
+    }
+  ]
+
+  pages.each do |test_case|
+    it "is navigated to from #{test_case[:dest]}" do
+      visit test_case[:origin]
+
+      # Click the link with text "See all delegators"
+      find("a", text: "See all delegators".upcase).click
+
+      # Verify the URL contains the expected href
+      expect(page.current_url).to include(test_case[:href]), "Expected URL to include '#{test_case[:href]}', but was #{page.current_url}"
+
+      # Verify the "Delegate" column in the "Staking Ledger - Epoch 1" table contains GENESIS_ACCOUNT_PK or "Self"
+      wait_until_table_loaded("Staking Ledger - Epoch 0")
+      delegate_cells = all(table_column_selector("Staking Ledger - Epoch 0", "Delegate".upcase))
+      delegate_cells.each do |cell|
+        cleaned_text = cell.text.gsub(/[\n+-]/, "")
+        expect(cleaned_text).to satisfy("be '#{Constants::GENESIS_ACCOUNT_PK}' or 'Self'") { |text|
+          text == Constants::GENESIS_ACCOUNT_PK || text == "Self"
+        }
+      end
+    end
+  end
+end
