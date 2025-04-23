f9c3cafdcaca0b0c016cd5d3584f22edd1fc3da3
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-23 15:29:57 -0300

    move staker leaderboard table test to ruby

--- cypress/e2e/tables/tests/table.analytics-staker-leaderboard_epoch_0.cy.js
+++
@@ -1,4 +0,0 @@
-// Import the main runTestSuite function and the specific test suite data
-import { runTestSuite } from "../run-test-suite";
-import testSuiteData from "../data/data.analytics-staker-leaderboard_epoch_0.js";
-runTestSuite(testSuiteData);
---
+++ spec/features/tables/staker_leaderboard_spec.rb
@@ -0,0 +1,28 @@
+# spec/features/tables/snarker_leaderboard_spec.rb
+require "spec_helper"
+
+RSpec.describe "Snarker Leaderboard table", type: :system do
+  let(:url) { "/analytics/staker-leaderboard?epoch=0" }
+  let(:heading) { "Staker Leaderboard" }
+  let(:columns) { [
+    "Username",
+    "Public Key",
+    "Canonical Blocks Produced",
+    "Supercharged Blocks Produced",
+    "Slots Produced",
+    "Orphan Rate",
+  ].map(&:upcase) }
+
+  before do
+    visit url
+    wait_until_table_loaded(heading)
+  end
+
+  it "has ordered columns" do
+    test_ordered_columns(heading, columns)
+  end
+
+  it "has sortable column 'Canonical Blocks Produced'" do
+    test_sortable_column(heading, "Canonical Blocks Produced", [nil, "NUM_CANONICAL_BLOCKS_PRODUCED_ASC", "NUM_CANONICAL_BLOCKS_PRODUCED_DESC"])
+  end
+end
