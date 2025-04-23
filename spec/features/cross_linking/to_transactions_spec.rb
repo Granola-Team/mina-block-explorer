4d817bfea8a429744f08eb889aa34452480bd13f
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-23 09:47:21 -0300

    cleanup

--- spec/features/cross_linking/to_transactions_spec.rb
+++ spec/features/cross_linking/to_transactions_spec.rb
@@ -28,4 +28,19 @@ RSpec.describe "Transaction spotlight navigation", type: :system do
       expect(page.current_url).to include("/commands/"), "Expected URL to include '/commands/', but was #{page.current_url}"
     end
   end
+
+  it "is navigated to with correct url params from /commands/user page by clicking link in 'Txn Hash'" do
+    visit "/commands/user?q-txn-hash=#{Constants::FIRST_TXN_HASH}"
+    wait_until_table_loaded("User Commands")
+
+    click_link_in_table_column("User Commands", "Txn Hash".upcase, 1)
+
+    wait_until_spotlight_loaded
+
+    within("section#spotlight-section table") do
+      canonical = find("tr", text: "Canonical")
+      data = canonical.find("td", match: :first)
+      expect(data.text).to eq("true"), "Expected 'Canonical' to be 'true', but found '#{data.text}'"
+    end
+  end
 end
