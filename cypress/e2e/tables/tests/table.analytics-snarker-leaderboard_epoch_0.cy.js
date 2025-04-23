0ec72d032e148ec1e0b6cdab62cde691437f2d36
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-04-23 15:14:46 -0300

    move snarker leaderboard table test over to ruby

--- cypress/e2e/tables/tests/table.analytics-snarker-leaderboard_epoch_0.cy.js
+++
@@ -1,6 +0,0 @@
-// import { runTestSuite } from "../run-test-suite";
-// import testSuiteData from "../data/data.analytics-snarker-leaderboard_epoch_0.js";
-// runTestSuite(testSuiteData);
-
-// TODO: commented out due to lack of snark activity immediately after hardfork
-xit("skip");
---
+++ spec/features/tables/snarker_leaderboard_spec.rb
@@ -0,0 +1,25 @@
+# spec/features/tables/snarker_leaderboard_spec.rb
+require "spec_helper"
+
+RSpec.describe "Snarker Leaderboard table", type: :system do
+  let(:url) { "/analytics/snarker-leaderboard?epoch=0" }
+  let(:heading) { "Snarker Leaderboard" }
+  let(:columns) { ["Username", "Public Key", "Total Fees", "Min Fee", "Max Fee", "Snarks Sold"].map(&:upcase) }
+
+  before do
+    visit url
+    wait_until_table_loaded(heading)
+  end
+
+  it "has ordered columns" do
+    test_ordered_columns(heading, columns)
+  end
+
+  it "has sortable column 'Max Fee'" do
+    test_sortable_column(heading, "Max Fee", [nil, "MAX_FEE_DESC", "MAX_FEE_ASC"])
+  end
+
+  it "has sortable column 'Total Fees'" do
+    test_sortable_column(heading, "Total Fees", [nil, "TOTAL_FEES_DESC", "TOTAL_FEES_ASC"])
+  end
+end
