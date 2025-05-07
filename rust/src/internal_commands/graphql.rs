64498fa39d537a7f3ebc8d54709a7f3c7551edaf
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-05-07 15:40:44 -0300

    move graphql folder into rust source code since it is used exclusively by rust code

--- rust/src/internal_commands/graphql.rs
+++ rust/src/internal_commands/graphql.rs
@@ -9,8 +9,8 @@ type Long = i32;
 
 #[derive(GraphQLQuery)]
 #[graphql(
-    schema_path = "../graphql/schemas/mina-explorer.graphql",
-    query_path = "../graphql/queries/internal_commands.graphql",
+    schema_path = "./graphql/schemas/mina-explorer.graphql",
+    query_path = "./graphql/queries/internal_commands.graphql",
     response_derives = "Serialize,PartialEq,Debug,Clone,Default",
     skip_serializing_none
 )]
