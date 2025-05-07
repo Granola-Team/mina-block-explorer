ccb7b912e9f749687e412dfe3f7754484ea25cd7
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-05-07 09:25:28 -0300

    round decimals on staker leaderboard to two places

--- ruby/spec/features/account_listing_symbol_spec.rb
+++ ruby/spec/features/account_listing_symbol_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe "Accounts listing section header", type: :system do
   it "updates when clicking on the 'Mina Accounts' menu item" do
     visit "/addresses/accounts?q-token=#{Constants::MINU_TOKEN_ADDRESS}"
-    expect(page).to have_content("MINU Token Accounts")
+    wait_until_table_loaded("MINU Token Accounts")
     find("a", text: "MINA ACCOUNTS").click
     expect(page).to have_content("MINA Token Accounts")
   end
