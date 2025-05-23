151e161ec8f8db90186c646a48042b81854b2780
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-05-23 11:13:11 -0300

    init staker leaderboard analytics module

--- rust/src/analytics/mod.rs
+++ rust/src/analytics/mod.rs
@@ -2,4 +2,5 @@ mod components;
 mod functions;
 pub mod models;
 pub mod page;
+mod staker_leaderboard;
 mod table_traits;
---
+++ rust/src/analytics/staker_leaderboard/graphql.rs
@@ -0,0 +1,13 @@
+use chrono::Utc;
+use graphql_client::GraphQLQuery;
+type DateTime = chrono::DateTime<Utc>;
+type Long = i32;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "./graphql/schemas/mina-explorer.graphql",
+    query_path = "./graphql/queries/staker_leaderboard.graphql",
+    response_derives = "Serialize,PartialEq,Debug,Clone,Default",
+    skip_serializing_none
+)]
+pub struct TopStakersQuery;
